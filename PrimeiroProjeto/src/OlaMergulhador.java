306b7ab359104e357f06edeaaae84c60101f6768
Moisés Guilherme <moisescomp@gmail.com>
2022-07-10 20:16:52 -0300

    Projeto usando IntelliJ IDA

--- PrimeiroProjeto/src/OlaMergulhador.java
+++ PrimeiroProjeto/src/OlaMergulhador.java
@@ -1,10 +1,40 @@
 public class OlaMergulhador {
 
     public static void main(String[] args) {
-        int idade = 10;
+        // Atalhos do IntelliJ IDEA
 
-        System.out.println(idade);
-        System.out.println("Fim");
+        int total = 10;
+        int gastos = 20;
+        boolean pendente = true;
+        // Code completion
+
+        // Live Templates
+        // fori
+        for (int i = 0; i < 10; i++) {
+            // soutv
+            System.out.println("gastos = " + i + " " + gastos);
+        }
+        // Posfix Completion
+        // total.sout
+        System.out.println(total);
+        // total.souv
+        System.out.println("total = " + total);
+        // pendente.if
+        if (pendente) {
+
+        }
+        // pendente.wihle
+        while (pendente) {
+
+        }
+        // total.fori
+        for (int i = 0; i < total; i++) {
+
+        }
+        // total.forr
+        for (int i = total; i > 0; i--) {
+
+        }
     }
 
 }
