e3409ac85e3ea3f852bb93946c7e2de7c3d6dc51
Moisés Guilherme <moisescomp@gmail.com>
2022-07-09 21:41:48 -0300

    Projeto usando IntelliJ IDA

---
+++ PrimeiroProjeto/src/OlaMergulhador.java
@@ -0,0 +1,10 @@
+public class OlaMergulhador {
+
+    public static void main(String[] args) {
+        int idade = 10;
+
+        System.out.println(idade);
+        System.out.println("Fim");
+    }
+
+}
