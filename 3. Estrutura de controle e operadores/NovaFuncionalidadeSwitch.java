8ec9247548d1b1354a0318095d5922337f6734b7
Moisés Guilherme <moisescomp@gmail.com>
2022-07-12 19:51:26 -0300

    Nova Funcionalidade Switch Java 14 a 17 LTS

--- 3. Estrutura de controle e operadores/NovaFuncionalidadeSwitch.java
+++ 3. Estrutura de controle e operadores/NovaFuncionalidadeSwitch.java
@@ -7,11 +7,35 @@ public class NovaFuncionalidadeSwitch {
 
     System.out.print("Digite um dia da semana (ex: seg, ter, qua, etc): ");
     String diaSemana = entrada.nextLine();
-    String horarioFuncionamento;
+    
+    System.out.print("Mês: ");
+    int mes = entrada.nextInt();
+
+    // switch expression   
+    /*
+    String horarioFuncionamento = switch (diaSemana) {
+      case "seg" -> "Fechado";
+      case "ter", "qua", "qui", "sex" -> "08:00 às 18:00";
+      case "sab", "dom" -> "08:00 às 12:00";
+      default -> "Dia inválido";
+    };
+    */
+    
+    System.out.printf("Horário de funcionamento: %s%n", switch (diaSemana) {
+      case "seg" -> {
+      	 if ( mes == 12){
+      	   yield "08:00 às 16:00";	
+      	 }
+      	 yield "Fechado";
+      }
+      case "ter", "qua", "qui", "sex" -> "08:00 às 18:00";
+      case "sab", "dom" -> "08:00 às 12:00";
+      default -> "Dia inválido";
+    });
 
     // Arrow label -> Nova funcionalidade do Switch no Java 14
     // Não tem break
-    
+    /*
     switch (diaSemana) {
       case "seg" -> horarioFuncionamento = "Fechado";
       case "ter", "qua", "qui", "sex" -> horarioFuncionamento = "08:00 às 18:00";
@@ -20,8 +44,9 @@ public class NovaFuncionalidadeSwitch {
          horarioFuncionamento = "Dia inválido";
       }      
     }
-
-    System.out.printf("Horário de funcionamento: %s%n", horarioFuncionamento);
+    */	
+    //System.out.printf("Horário de funcionamento: %s%n", horarioFuncionamento);
+    
   }
 
 }
