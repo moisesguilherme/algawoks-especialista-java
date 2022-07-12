fd47d0b51d8c48d38ceb2f8dfad2a8458c5804ab
Moisés Guilherme <moisescomp@gmail.com>
2022-07-12 20:07:01 -0300

    Operador ternário

---
+++ 3. Estrutura de controle e operadores/CalculadoraImpostos.java
@@ -0,0 +1,29 @@
+public class CalculadoraImpostos {
+
+  public static void main(String[] args) {
+    char tipoNotaFiscal = 'S';
+    double totalFaturado = 1200.30;
+
+
+    /*
+    double valorImpostos;
+    if (tipoNotaFiscal == 'S') {
+      valorImpostos = totalFaturado * 0.16;
+    } else {
+      valorImpostos = totalFaturado * 0.35;
+    }*/
+
+    /*double valorImpostos = tipoNotaFiscal == 'S' 
+       ? totalFaturado * 0.16 
+       : totalFaturado * 0.35;        
+    */
+    
+    // Deixar o código mais legível
+    double taxaImpostos = tipoNotaFiscal == 'S' ? 0.16 : 0.35;
+    double valorImpostos = totalFaturado * taxaImpostos;
+    
+    System.out.printf("Total faturado: R$%.2f%n", totalFaturado);
+    System.out.printf("Valor dos impostos: R$%.2f%n", valorImpostos);
+  }
+
+}
