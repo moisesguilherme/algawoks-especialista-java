c3f61cf4cb17960f8a30914d8dfd3b0d30715f03
Moisés Guilherme <moisescomp@gmail.com>
2022-07-13 06:42:31 -0300

    Estrutura de repetição for

---
+++ 3. Estrutura de controle e operadores/EstruturaRepeticaoFor.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class EstruturaRepeticaoFor {
+
+  public static void main(String[] args) {
+    
+    /*
+    for (int i = 1; i <= 10; i++) {
+    	System.out.println("i: " + i);
+    }
+    */
+    
+    
+    for (int i = 10; i > 0; i--) {
+    	System.out.println("i: " + i);
+    }
+    
+    /*
+    for(;;) {
+    	System.out.println("teste");
+    }*/
+
+
+  }
+
+}
