c22b9542c33b67dcbd093d10218f738bf0e68dfe
Moisés Guilherme <moisescomp@gmail.com>
2022-07-13 07:08:53 -0300

    Estrutura de repetição while

--- 3. Estrutura de controle e operadores/ControlePesoAeronave.java
+++ 3. Estrutura de controle e operadores/ControlePesoAeronave.java
@@ -8,17 +8,42 @@ public class ControlePesoAeronave {
     System.out.print("Peso máximo da aeronave: ");
     int pesoMaximo = entrada.nextInt();
     
-    System.out.print("Quantidade de passageiros: ");
+    /*System.out.print("Quantidade de passageiros: ");
     int totalPassageiros = entrada.nextInt();
+    */
     
     int pesoTotalPassageiros = 0;
     
+    /*
     for (int passageiroAtual = 1; passageiroAtual <= totalPassageiros; passageiroAtual++) {
        System.out.printf("Peso do passageiro #%d: ", passageiroAtual);
        int pesoPassageiro = entrada.nextInt();
        
        pesoTotalPassageiros += pesoPassageiro;
+    }*/
+    
+    /*int passageiroAtual = 1;
+    
+    while(passageiroAtual <= totalPassageiros) {
+       System.out.printf("Peso do passageiro #%d: ", passageiroAtual);
+       int pesoPassageiro = entrada.nextInt();
+       
+       pesoTotalPassageiros += pesoPassageiro;
+       passageiroAtual++;    
+    }*/
+    
+    boolean incluirNovoPassageiro = true;
+    
+    // while é ideal quando vc não tem informação de quando vai parar
+    while(pesoTotalPassageiros <= pesoMaximo && incluirNovoPassageiro) {
+       System.out.print("Peso do passageiro : ");
+       int pesoPassageiro = entrada.nextInt();
+       
+       pesoTotalPassageiros += pesoPassageiro;
+       System.out.print("Incluir novo passageiro? ");
+       incluirNovoPassageiro = entrada.nextBoolean();
     }
+    
     
     System.out.printf("Peso máximo da aeronave: %d kg%n", pesoMaximo);
     System.out.printf("Peso total dos passageiros: %d kg%n", pesoTotalPassageiros);
