a76828c2f8f6768104429b2063387bec54bd3098
Moisés Guilherme <moisescomp@gmail.com>
2022-07-28 07:07:32 -0300

    Código base para precificacao

---
+++ 5. Mergulhando em orientacao a objetos/precificacao/src/ServicoDePrecificacao.java
@@ -0,0 +1,9 @@
+public class ServicoDePrecificacao {
+
+    void calcularPrecoVenda(double precoCusto) {
+        // adiciona 20% de margem de lucro
+        //precoCusto = precoCusto * 1.20;
+        // faz várias outras coisas
+    }
+
+}
