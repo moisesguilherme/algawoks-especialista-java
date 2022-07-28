9c90bb0459ac1907ecece30decdc9829b90f8112
Moisés Guilherme <moisescomp@gmail.com>
2022-07-28 06:58:21 -0300

    Métodos que alteram variável de instância

--- 5. Mergulhando em orientacao a objetos/companhia-aerea/src/Aeronave.java
+++ 5. Mergulhando em orientacao a objetos/companhia-aerea/src/Aeronave.java
@@ -7,4 +7,20 @@ public class Aeronave {
     int calcularAssentosDisponiveis() {
         return totalAssentos - assentosReservados;
     }
+
+    void reservarAssentos(int numeroAssentos){
+        if (ativo) {
+            assentosReservados += numeroAssentos;
+        }else {
+            // existe uma forma de fazer isso com (excecões)
+            System.out.println("Aeronave desativada. Assentos não reservados.");
+        }
+    }
+    void desativar() {
+        ativo = false;
+    }
+
+    void ativar() {
+        ativo = true;
+    }
 }
--- 5. Mergulhando em orientacao a objetos/companhia-aerea/src/Principal.java
+++ 5. Mergulhando em orientacao a objetos/companhia-aerea/src/Principal.java
@@ -4,10 +4,29 @@ public class Principal {
         Aeronave aviaoGol = new Aeronave();
         aviaoGol.totalAssentos = 100;
 
+        // vai resolver isso com encapsulamento
+        //aviaoGol.ativo = false;
+        aviaoGol.desativar();
+        aviaoGol.ativar();
+
+        aviaoGol.reservarAssentos(10);
 
         System.out.printf("GOL (%s): %d assentos disponíveis%n",
                 aviaoGol.ativo ? "Ativo" : "Inativo",
                 aviaoGol.calcularAssentosDisponiveis());
+
+
+
+        // -----
+
+        Aeronave aviaoLatam = new Aeronave();
+        aviaoLatam.totalAssentos = 120;
+
+        aviaoLatam.reservarAssentos(5);
+
+        System.out.printf("LATAM (%s): %d assentos disponíveis%n",
+                aviaoLatam.ativo ? "Ativo" : "Inativo",
+                aviaoLatam.calcularAssentosDisponiveis());
 
     }
 
