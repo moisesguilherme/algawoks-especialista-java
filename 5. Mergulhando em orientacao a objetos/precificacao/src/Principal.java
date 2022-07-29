6d31a99f77391a3e9c2301c787655f74195af5e7
Moisés Guilherme <moisescomp@gmail.com>
2022-07-29 06:43:07 -0300

    Usando a palavra-chave this

--- 5. Mergulhando em orientacao a objetos/precificacao/src/Principal.java
+++ 5. Mergulhando em orientacao a objetos/precificacao/src/Principal.java
@@ -2,14 +2,17 @@ public class Principal {
 
     public static void main(String[] args) {
         Produto novoProduto = new Produto();
-        novoProduto.precoCusto = 100;
+        // novoProduto.precoCusto = 100;
+        novoProduto.alterarPrecoCusto(80);
 
         ServicoDePrecificacao servicoDePrecificacao = new ServicoDePrecificacao();
         // Quando passar o precoCompraFornecedor vai alterar o valor ou não?
         // não ele passa o valor e não a referência da variável
         servicoDePrecificacao.definirPrecoVenda(novoProduto, 20);
 
+        System.out.printf("Preço de custo: %.2f%n", novoProduto.precoCusto);
         System.out.printf("Preço de venda: %.2f%n", novoProduto.precoVenda);
     }
 
 }
+
--- 5. Mergulhando em orientacao a objetos/precificacao/src/Produto.java
+++ 5. Mergulhando em orientacao a objetos/precificacao/src/Produto.java
@@ -2,4 +2,8 @@ public class Produto {
     double precoCusto;
     double precoVenda;
 
+    void alterarPrecoCusto(double precoCusto) {
+        this.precoCusto = precoCusto;
+    }
+
 }
