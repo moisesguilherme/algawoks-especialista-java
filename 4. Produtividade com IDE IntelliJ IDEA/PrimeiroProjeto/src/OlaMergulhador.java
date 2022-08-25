7ddcc1d298758ab142680462f2cfaaf610ee5420
Moisés Guilherme <moisescomp@gmail.com>
2022-08-25 06:55:52 -0300

    Pricipais Atalhos da IDE IntelliJ

--- 4. Produtividade com IDE IntelliJ IDEA/PrimeiroProjeto/src/OlaMergulhador.java
+++ 4. Produtividade com IDE IntelliJ IDEA/PrimeiroProjeto/src/OlaMergulhador.java
@@ -2,9 +2,19 @@ public class OlaMergulhador {
 
     public static void main(String[] args) {
         // Atalhos do IntelliJ IDEA
+        // CTRL + Alt + L -> Formatar
+        // Shift + f6 selecionando a variável/classe para mudar o nome em _todo o programa
+        // Shift + 10 -> para rodar o projeto ativo (no select run)
+        // Ctrl + Shift + 10 -> para rodar o projeto aberto
+        // Ctrl + N -> achar uma classe (pesquisar)
+        // Ctrl + Shift + N -> achar um arquivo
+        // Ctrl + Shift + A -> achar actions
 
-        int total = 10;
-        int gastos = 20;
+        int totalTeste = 3; // Identar uma linha CTRL + ALT + i;
+        int teste2; // Duplicar CTRL + D;
+        int teste3; // Apagar uma linha CTRL + Y
+        // Shift + enter quebrar a linha sem dividir, com cursor no meio da palavra.
+
         boolean pendente = true;
         // Code completion
 
@@ -12,31 +22,41 @@ public class OlaMergulhador {
         // fori
         for (int i = 0; i < 10; i++) {
             // soutv
-            System.out.println("gastos = " + i + " " + gastos);
+            System.out.println("gastos = " + i + " " + totalTeste);
         }
+
         // Posfix Completion
         // total.sout
-        System.out.println(total);
+        System.out.println(totalTeste);
         // total.souv
-        System.out.println("total = " + total);
+        System.out.println("total = " + totalTeste);
         // pendente.if
         if (pendente) {
 
         }
-        // pendente.wihle
+        // pendente.while
         while (pendente) {
 
         }
         // total.fori
-        for (int i = 0; i < total; i++) {
+        for (int i = 0; i < totalTeste; i++) {
 
         }
         // total.forr
-        for (int i = total; i > 0; i--) {
+        for (int i = totalTeste; i > 0; i--) {
 
         }
 
+        // Ver cursor para alterar mais de uma linha
+        System.out.println("Alterar texto");
+        System.out.println("Alterar texto");
+        System.out.println("Alterar texto");
+        System.out.println("Alterar texto");
 
+        //ctrl + j (lista de template)
+        for (int i = 0; i < totalTeste ; i++) {
+
+        }
     }
 
 }
