f374a499249a1c54d128ccb9118fb1be585494af
Moisés Guilherme <moisescomp@gmail.com>
2022-12-15 20:25:54 -0300

    5.38. Sobrecarga de métodos

--- 5. Mergulhando em orientacao a objetos/portaria/src/CadastroPortaria.java
+++ 5. Mergulhando em orientacao a objetos/portaria/src/CadastroPortaria.java
@@ -1,5 +1,21 @@
 public class CadastroPortaria {
 
+    static final int TEMPO_EXPIRACAO_EM_MESES = 1;
+
+    // Overloading - sobrecarga de métodos
+    // mesmo nome com parâmetros diferentes
+    void cadastrar(final Visitante visitante) {
+        // this chama método de instância - opcional
+        this.cadastrar(visitante, TEMPO_EXPIRACAO_EM_MESES);
+    }
+    // precisa mudar o tipo para ser diferente o outro método
+    // para sobrecarga não pode ter a mesma assinatura do método
+    // void cadastrar(Visitante, int)
+
+    void cadastrar(final Visitante visitante, final long senha) {
+    }
+
+
     void cadastrar(final Visitante visitante, final int tempoExpiracaoEmMeses) {
 
         final int tempoExpiracaoEmDias = tempoExpiracaoEmMeses * 30;
@@ -7,6 +23,4 @@ public class CadastroPortaria {
         System.out.printf("Visitante %s cadastrado para %d dias%n",
                 visitante.nome, tempoExpiracaoEmDias);
     }
-
-
 }
--- 5. Mergulhando em orientacao a objetos/portaria/src/Principal.java
+++ 5. Mergulhando em orientacao a objetos/portaria/src/Principal.java
@@ -6,7 +6,7 @@ public class Principal {
         novoVisitante.idade = 15;
 
         CadastroPortaria cadastroPortaria = new CadastroPortaria();
-        cadastroPortaria.cadastrar(novoVisitante, 10);
+        cadastroPortaria.cadastrar(novoVisitante, 2L);
 
     }
 
