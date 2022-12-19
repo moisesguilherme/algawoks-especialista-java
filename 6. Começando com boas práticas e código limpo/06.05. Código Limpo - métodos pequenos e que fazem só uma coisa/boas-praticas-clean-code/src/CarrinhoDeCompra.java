94aa94939f3a75bca4e013fc9e3028006556802b
Moisés Guilherme <moisescomp@gmail.com>
2022-12-19 07:19:08 -0300

    6.5. Código Limpo: métodos pequenos e que fazem só uma coisa - depois refatoração

--- 6. Começando com boas práticas e código limpo/06.05. Código Limpo - métodos pequenos e que fazem só uma coisa/boas-praticas-clean-code/src/CarrinhoDeCompra.java
+++ 6. Começando com boas práticas e código limpo/06.05. Código Limpo - métodos pequenos e que fazem só uma coisa/boas-praticas-clean-code/src/CarrinhoDeCompra.java
@@ -1,19 +1,45 @@
 public class CarrinhoDeCompra {
 
+    static final double TAXA_SERVICO_PADRAO = 0.10;
+
     int quantidadeItens;
     Produto produto;
 
+    // Nível mais alto (Níveis de abstracão dos métodos)
+    // Esse método possui 3 etapas, apenas executa as etapas não faz ela.
+    // Não trabalha com os detalhes da compra
     void realizarCompra(String numeroCartao) {
-        double subtotal = produto.precoUnitario * quantidadeItens;
-        double taxaServico = subtotal * 0.10;
-        double valorTotal = subtotal + taxaServico;
+        double valorTotal = calcularValorTotal();
 
         boolean pagamentoAutorizado = GatewayPagamento.autorizarPagamento(numeroCartao, valorTotal);
 
         if(pagamentoAutorizado){
-            System.out.println("RECIBO DE PAGAMENTO");
-            System.out.println("Valor total: R$%.2f%n", valorTotal);
+            // Ideal apenas uma linha no if (transformar em método)
+            imprimirRecibo(valorTotal);
         }
+    }
+
+    private static void imprimirRecibo(double valorTotal) {
+        System.out.println("RECIBO DE PAGAMENTO");
+        System.out.println("Valor total: R$%.2f%n", valorTotal);
+    }
+
+    // Nível intermediário, ele nào faz a contas, calcular definitivamente, ainda executa e chama outros métodos.
+    double calcularValorTotal() {
+        double subtotal = calcularSubtotal();
+        double taxaServico = calcularTaxaServico(subtotal);
+        double valorTotal = subtotal + taxaServico;
+
+        return valorTotal;
+    }
+
+    // Nível mais baixo de método, faz a conta.
+    private static double calcularTaxaServico(double subtotal) {
+        return subtotal * TAXA_SERVICO_PADRAO;
+    }
+
+    private double calcularSubtotal() {
+        return produto.precoUnitario * quantidadeItens;
     }
 
 
