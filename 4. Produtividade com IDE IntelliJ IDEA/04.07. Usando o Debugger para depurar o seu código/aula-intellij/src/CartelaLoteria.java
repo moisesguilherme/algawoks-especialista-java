fe8f21a5635efeef3d6635094c1e3ce7ebaa95c1
Moisés Guilherme <moisescomp@gmail.com>
2022-12-23 07:32:12 -0300

    4.10. Debugger: watches e logging

--- 4. Produtividade com IDE IntelliJ IDEA/04.07. Usando o Debugger para depurar o seu código/aula-intellij/src/CartelaLoteria.java
+++ 4. Produtividade com IDE IntelliJ IDEA/04.07. Usando o Debugger para depurar o seu código/aula-intellij/src/CartelaLoteria.java
@@ -23,6 +23,12 @@ public class CartelaLoteria {
 
         // ver variável, condicão, ícone calculadora.
 
+        // Adicionar um watch ctrl + shift + enter
+
+        // pode fazer um log no break-point, desmarcar o suspend (para na linha) e colcoar
+        // - Evaluate and log.
+        // "Dentro do loop" + totalNumeros + " - " + i
+        
         for (int i = 1; i <= totalNumeros; i++) {
             if (i < 10) {
                 System.out.print("0");
