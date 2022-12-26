ece3e404bf385e27ba95d81d3306f4303031d49f
Moisés Guilherme <moisescomp@gmail.com>
2022-12-26 07:20:51 -0300

    4.13. Consistência no estilo de codificação com EditorConfig

--- 4. Produtividade com IDE IntelliJ IDEA/04.07. Usando o Debugger para depurar o seu código/aula-intellij/src/CartelaLoteria.java
+++ 4. Produtividade com IDE IntelliJ IDEA/04.07. Usando o Debugger para depurar o seu código/aula-intellij/src/CartelaLoteria.java
@@ -3,31 +3,8 @@ public class CartelaLoteria {
     public static void main(String[] args) {
         System.out.println("Imprimindo cartela...\n");
 
-        // ctrl + f8 (criar o break point)
-        // shift + 9 (Entrar no modo debug)
-        // f8 - step-over
-        // f9 - resume program
-        // btn direito run cursor (para ir até o cursor no debug)
-
         int totalNumeros = 60;
         int numerosPorLinha = 10;
-
-        // Criar break point
-        // - possibilidade de resume break point
-        // - mute break-point simbolo/icone - bola vermelha - com um cortado.
-        // - colocar condicao no break-point (btn direito no íconee do break-point)
-
-        // Alterar a variável em tempo de debug.
-        // - em Debugger (menu inferior) na lista de variáveis do painel, clicar em btn direito
-        //   set value, ou F2
-
-        // ver variável, condicão, ícone calculadora.
-
-        // Adicionar um watch ctrl + shift + enter
-
-        // pode fazer um log no break-point, desmarcar o suspend (para na linha) e colcoar
-        // - Evaluate and log.
-        // "Dentro do loop" + totalNumeros + " - " + i
 
         for (int i = 1; i <= totalNumeros; i++) {
             if (i < 10) {
