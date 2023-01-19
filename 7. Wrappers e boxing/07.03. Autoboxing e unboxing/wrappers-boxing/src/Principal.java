70819b5510e25d42485141d930ddef02648af814
Moisés Guilherme <moisescomp@gmail.com>
2023-01-19 20:12:15 -0300

    7.3. Autoboxing e unboxing - depois

--- 7. Wrappers e boxing/07.03. Autoboxing e unboxing/wrappers-boxing/src/Principal.java
+++ 7. Wrappers e boxing/07.03. Autoboxing e unboxing/wrappers-boxing/src/Principal.java
@@ -1,7 +1,19 @@
 public class Principal {
 
     public static void main(String[] args) {
-        Integer diasEntrega = Integer.valueOf(30);
+
+        // Remove Wrapper (boxing)
+        // a partir do java 5 não precisa fazer o wrapper (tem a funcionalidade chamada auto-boxing)
+        // o compilador embrulha o valor em um objeto tipo integer.
+        // Integer diasEntrega = Integer.valueOf(30);
+        // equivalente a
+        Integer diasEntrega = 30;
+
+        // remover o unboxing
+        //int diasEntregaInt = diasEntrega.intValue();
+        // aqui o compilador faz o auto-unboxing.
+        int diasEntregaInt = diasEntrega;
+
     }
 
 }
