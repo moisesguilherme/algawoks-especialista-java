abb12a5668c68cbacd12b59efb2ea88945300371
Moisés Guilherme <moisescomp@gmail.com>
2023-04-08 15:41:04 -0300

    7.4. Comparando wrappers - antes

---
+++ 7. Wrappers e boxing/07.04. Comparando wrappers/wrappers-boxing/src/Principal.java
@@ -0,0 +1,12 @@
+public class Principal {
+
+    public static void main(String[] args) {
+        int numero1 = 127;
+        int numero2 = 127;
+
+
+        // com int vai dar true comparando.
+        System.out.println(numero1 == numero2);
+    }
+
+}
