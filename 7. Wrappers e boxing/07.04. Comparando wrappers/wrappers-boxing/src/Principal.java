b460ee55f2069d40327bd258735e76df3d781a34
Moisés Guilherme <moisescomp@gmail.com>
2023-04-08 16:04:37 -0300

    integerCache, não podemos comparar valores com Integer

--- 7. Wrappers e boxing/07.04. Comparando wrappers/wrappers-boxing/src/Principal.java
+++ 7. Wrappers e boxing/07.04. Comparando wrappers/wrappers-boxing/src/Principal.java
@@ -1,11 +1,14 @@
 public class Principal {
 
     public static void main(String[] args) {
-        int numero1 = 127;
-        int numero2 = 127;
-
+        Integer numero1 = 128;
+        Integer numero2 = 128;
 
         // com int vai dar true comparando.
+        // com o Integer vai comparar objetos, não o conteúdo do objeto.
+        // o número1 aponta para o mesmo endereco de memoria, como:
+        // existe o integerCache na classe Integer, que vai instanciar o mesmo objeto que tiver o mesmo valor.
+        // na faixa de -128 até 127
         System.out.println(numero1 == numero2);
     }
 
