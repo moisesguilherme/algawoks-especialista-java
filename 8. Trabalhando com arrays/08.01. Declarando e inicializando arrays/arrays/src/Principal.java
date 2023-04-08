99eb786b47456244306c1cb0e6634f0be851f475
Moisés Guilherme <moisescomp@gmail.com>
2023-04-08 19:12:47 -0300

    8.1. Declarando e inicializando arrays

---
+++ 8. Trabalhando com arrays/08.01. Declarando e inicializando arrays/arrays/src/Principal.java
@@ -0,0 +1,16 @@
+public class Principal {
+
+    public static void main(String[] args) {
+
+        // Funciona com [] no tipo e no nome da variável int[] notas[]
+        // int[] notas = new int[5];
+
+        // int[] notas;
+        // notas = new int[]{8, 5, 4, 9, 10};
+
+        // outra forma de inicializar um array
+        int[] notas = {8, 5, 4, 9, 10};
+
+    }
+
+}
