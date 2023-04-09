42ddd33e0cf92b676891ccb60ddbc34cb7760619
Moisés Guilherme <moisescomp@gmail.com>
2023-04-09 15:46:57 -0300

    8.3. Iterando em arrays - enhanced-for

--- 8. Trabalhando com arrays/08.03. Iterando em arrays/arrays/src/Calculadora.java
+++ 8. Trabalhando com arrays/08.03. Iterando em arrays/arrays/src/Calculadora.java
@@ -2,9 +2,18 @@ public class Calculadora {
 
     static double calcularMedia(int[] numeros) {
         int total = 0;
-
+        // atalho digitar: numeros.fori
+        // se precisar do indice é melhor usar esse for
+        /*
         for(int i = 0; i < numeros.length; i++){
             total += numeros[i];
+        }
+        */
+
+        // java 5 enhanced-for
+        // for - se precisar ir até o final
+        for (int numero : numeros) {
+            total += numero;
         }
 
         return (double) total / numeros.length;
