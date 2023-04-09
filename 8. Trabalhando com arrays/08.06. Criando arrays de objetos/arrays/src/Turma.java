6deb2a3e3efac7a14ecfa9aa11420aba40ea58f0
Moisés Guilherme <moisescomp@gmail.com>
2023-04-09 19:02:44 -0300

    8.6. Criando arrays de objetos - depois

--- 8. Trabalhando com arrays/08.06. Criando arrays de objetos/arrays/src/Turma.java
+++ 8. Trabalhando com arrays/08.06. Criando arrays de objetos/arrays/src/Turma.java
@@ -2,5 +2,10 @@ public class Turma {
 
     String identificacao;
     String nomeProfessora;
+    //Não é uma boa prática colcoar releacão alunos na classe Turma,
+    //melhor criar uma classe aluno
+    //String[] nomesAlunos;
+    //int[] idadesAlunos;
+    Aluno[] alunos;
 
 }
