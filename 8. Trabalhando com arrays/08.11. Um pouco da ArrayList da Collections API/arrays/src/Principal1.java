cb8391bc64ba4e96e5b749dfb1e3e0a1d89c0399
Moisés Guilherme <moisescomp@gmail.com>
2023-04-15 20:03:16 -0300

    8.11. Um pouco da ArrayList da Collections API  - Usando collections

--- 8. Trabalhando com arrays/08.11. Um pouco da ArrayList da Collections API/arrays/src/Principal1.java
+++ 8. Trabalhando com arrays/08.11. Um pouco da ArrayList da Collections API/arrays/src/Principal1.java
@@ -3,13 +3,23 @@ import java.util.ArrayList;
 public class Principal1 {
 
     public static void main(String[] args) {
-        String[] alunos = new String[2];
-        alunos[0] = "João";
-        alunos[1] = "Maria";
+        // String[] alunos = new String[2];
+        // Estrutura de dados colections
+        // ArrayList<> - <> diamon operator não tem a necessidade de colocar string no new ArrayList<String>
+        ArrayList<String> alunos = new ArrayList<>();
 
+        //alunos[0] = "João";
+        //alunos[1] = "Maria";
+        alunos.add("João");
+        alunos.add("Maria");
 
-        for (int i = 0; i < alunos.length; i++) {
+        /*for (int i = 0; i < alunos.length; i++) {
             String aluno = alunos[i];
+            System.out.println(aluno);
+        }*/
+
+        for (int i = 0; i < alunos.size(); i++) {
+            String aluno = alunos.get(i);
             System.out.println(aluno);
         }
 
