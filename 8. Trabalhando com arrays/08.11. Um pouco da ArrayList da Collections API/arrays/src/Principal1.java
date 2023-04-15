34839e0e99e5e12f0098683639dbd8291adea9aa
Moisés Guilherme <moisescomp@gmail.com>
2023-04-15 20:05:49 -0300

    8.11. Um pouco da ArrayList da Collections API - enhenced for

--- 8. Trabalhando com arrays/08.11. Um pouco da ArrayList da Collections API/arrays/src/Principal1.java
+++ 8. Trabalhando com arrays/08.11. Um pouco da ArrayList da Collections API/arrays/src/Principal1.java
@@ -3,29 +3,20 @@ import java.util.ArrayList;
 public class Principal1 {
 
     public static void main(String[] args) {
-        // String[] alunos = new String[2];
-        // Estrutura de dados colections
-        // ArrayList<> - <> diamon operator não tem a necessidade de colocar string no new ArrayList<String>
         ArrayList<String> alunos = new ArrayList<>();
-
-        //alunos[0] = "João";
-        //alunos[1] = "Maria";
         alunos.add("João");
         alunos.add("Maria");
-
-        /*for (int i = 0; i < alunos.length; i++) {
-            String aluno = alunos[i];
-            System.out.println(aluno);
-        }*/
 
         for (int i = 0; i < alunos.size(); i++) {
             String aluno = alunos.get(i);
             System.out.println(aluno);
         }
 
-//        for (String aluno : alunos) {
-//            System.out.println(aluno);
-//        }
+        // enhenced for
+        // alunos for
+        for (String aluno : alunos) {
+            System.out.println(aluno);
+        }
     }
 
 }
