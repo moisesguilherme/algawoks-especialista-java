a55fd822a13b0695e955e037dbec9ebc591f2e1a
Moisés Guilherme <moisescomp@gmail.com>
2023-04-15 20:41:01 -0300

    8.10. Desafio: arrays - código antes

---
+++ 8. Trabalhando com arrays/08.10. Desafio - arrays/nome-do-mes/src/Calendario.java
@@ -0,0 +1,21 @@
+public class Calendario {
+
+    static String obterNomeMes(int numeroMes) {
+        return switch (numeroMes) {
+            case 1 -> "Janeiro";
+            case 2 -> "Fevereiro";
+            case 3 -> "Março";
+            case 4 -> "Abril";
+            case 5 -> "Maio";
+            case 6 -> "Junho";
+            case 7 -> "Julho";
+            case 8 -> "Agosto";
+            case 9 -> "Setembro";
+            case 10 -> "Outubro";
+            case 11 -> "Novembro";
+            case 12 -> "Dezembro";
+            default -> null;
+        };
+    }
+
+}
