1292f3bb26c3d41eaf73cd5aec6799b689b04ad7
Moisés Guilherme <moisescomp@gmail.com>
2023-04-16 13:40:14 -0300

    8.12. Desafio: ArrayList - depois

--- 8. Trabalhando com arrays/08.12. Desafio - ArrayList/cardapio/src/Cardapio.java
+++ 8. Trabalhando com arrays/08.12. Desafio - ArrayList/cardapio/src/Cardapio.java
@@ -1,22 +1,16 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Cardapio {
 
-    ItemCardapio[] itens = new ItemCardapio[0];
+    ArrayList<ItemCardapio> itens = new ArrayList<>();
 
     void adicionarItem(ItemCardapio item) {
-        itens = Arrays.copyOf(itens, itens.length + 1);
-        itens[itens.length - 1] = item;
+        itens.add(item);
     }
 
     void removerItem(int indice) {
-        ItemCardapio[] novosItens = new ItemCardapio[itens.length - 1];
-
-        System.arraycopy(itens, 0, novosItens, 0, indice);
-        System.arraycopy(itens, indice + 1,
-                novosItens, indice, novosItens.length - indice);
-
-        itens = novosItens;
+        itens.remove(indice);
     }
 
     void imprimirItensCardapio(double precoMinimo, double precoMaximo) {
