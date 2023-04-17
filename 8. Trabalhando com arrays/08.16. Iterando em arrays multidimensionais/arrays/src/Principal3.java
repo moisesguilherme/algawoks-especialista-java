d78ac0e87beaf9913c2f22497d4999070ed3a29d
Moisés Guilherme <moisescomp@gmail.com>
2023-04-17 20:29:53 -0300

    8.16. Iterando em arrays multidimensionais - antes

---
+++ 8. Trabalhando com arrays/08.16. Iterando em arrays multidimensionais/arrays/src/Principal3.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class Principal3 {
+
+    public static void main(String[] args) {
+        String[][] todasCidades = new String[3][];
+        todasCidades[0] = new String[3];
+        todasCidades[0][0] = "Uberlândia";
+        todasCidades[0][1] = "Uberaba";
+        todasCidades[0][2] = "Belo Horizonte";
+
+        todasCidades[1] = new String[2];
+        todasCidades[1][0] = "São Paulo";
+        todasCidades[1][1] = "Ribeirão Preto";
+
+        todasCidades[2] = new String[1];
+        todasCidades[2][0] = "Fortaleza";
+
+    }
+
+}
