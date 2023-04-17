dc3f5901685c1cf743e67500739a2bca2cc7c3f0
Moisés Guilherme <moisescomp@gmail.com>
2023-04-17 20:53:20 -0300

    8.16. Iterando em arrays multidimensionais - depois

--- 8. Trabalhando com arrays/08.16. Iterando em arrays multidimensionais/arrays/src/Principal3.java
+++ 8. Trabalhando com arrays/08.16. Iterando em arrays multidimensionais/arrays/src/Principal3.java
@@ -16,6 +16,27 @@ public class Principal3 {
         todasCidades[2] = new String[1];
         todasCidades[2][0] = "Fortaleza";
 
+        /*
+        for (int i = 0; i < todasCidades.length; i++) {
+            // todas as linhas
+            System.out.println(Arrays.toString(todasCidades[i]));
+        }
+        */
+
+        /*for (int i = 0; i < todasCidades.length; i++) {
+            for (int j = 0; j < todasCidades[i].length; j++) { // i primeira dimensão
+                System.out.printf("[%d][%d] = Cidade: %s%n", i, j, todasCidades[i][j]); // j segunda dimensão
+            }
+        }*/
+        
+        //Enhenced for (não trabalha com indice)
+        for (String[] cidadesPorEstados : todasCidades) {
+            for (String cidade : cidadesPorEstados) {
+                System.out.println("Cidade: " + cidade);
+            }
+        }
+        
+
     }
 
 }
