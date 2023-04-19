18f27248338c7fe035035461d62f3feba4e1658f
Moisés Guilherme <moisescomp@gmail.com>
2023-04-18 21:37:15 -0300

    8.18. Criando métodos com argumentos variáveis com Varargs - depois

--- 8. Trabalhando com arrays/08.18. Criando métodos com argumentos variáveis com Varargs/cobranca/src/Principal.java
+++ 8. Trabalhando com arrays/08.18. Criando métodos com argumentos variáveis com Varargs/cobranca/src/Principal.java
@@ -10,8 +10,18 @@ public class Principal {
         // String[] emailsCobranca = {"joao@gmail.com", "maria@gmail.com"};
         // servicoDeCobranca.pagar(fatura, emailsCobranca);
 
-        servicoDeCobranca.pagar(fatura, new String[] {"joao@gmail.com", "maria@gmail.com"});
+        // servicoDeCobranca.pagar(fatura, new String[] {"joao@gmail.com", "maria@gmail.com"});
 
+        // servicoDeCobranca.pagar(fatura, new String[]);
+        // servicoDeCobranca.pagar(fatura, new String[]{});
+
+        //varargs
+        // benefício, simplifica a chamada do método, não precisa instanciar
+        //servicoDeCobranca.pagar(fatura, "joao@gotocode.com", "maria@gotocode.com.br");
+        // Não precisa passar, vai gerar um array vazio
+        servicoDeCobranca.pagar(fatura);
+
+        servicoDeCobranca.teste(1, 2, 3, 4, 5);
     }
 
 }
--- 8. Trabalhando com arrays/08.18. Criando métodos com argumentos variáveis com Varargs/cobranca/src/ServicoDeCobranca.java
+++ 8. Trabalhando com arrays/08.18. Criando métodos com argumentos variáveis com Varargs/cobranca/src/ServicoDeCobranca.java
@@ -2,13 +2,21 @@ import java.util.Arrays;
 
 public class ServicoDeCobranca {
 
-    void pagar(Fatura fatura, String[] emailsCobranca) {
+    //Varargs
+    // restricao usando o varargs, ele precisa ser o último parâmetro do seu método.
+    // String... emailsCobranca
+    void pagar(Fatura fatura, String... emailsCobranca) {
+        System.out.println(Arrays.toString(emailsCobranca));
         System.out.printf("Fatura %d, no valor total de R$%.2f, foi paga!%n",
                 fatura.numero, fatura.valorTotal);
 
         for (String email : emailsCobranca) {
             System.out.printf("Fatura %d enviada para %s%n", fatura.numero, email);
         }
+    }
+
+    void teste(int... numeros){
+        System.out.println(Arrays.toString(numeros));
     }
 
 }
