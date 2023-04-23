369651dfa0b95208f541550986d669c4128e602f
Moisés Guilherme <moisescomp@gmail.com>
2023-04-23 19:56:34 -0300

    10.15. Importando membros estáticos (static import) - depois

--- 10. Construtores, pacotes e visibilidade/10.15. Importando membros estáticos (static import)/matematica-util/src/Principal.java
+++ 10. Construtores, pacotes e visibilidade/10.15. Importando membros estáticos (static import)/matematica-util/src/Principal.java
@@ -1,7 +1,5 @@
-import com.algaworks.matematica.CalculadoraArea;
-
 // adicionou o import static
-import static com.algaworks.matematica.CalculadoraArea.calcularAreaQuadrado;
+import static com.algaworks.matematica.CalculadoraArea.*;
 
 public class Principal {
 
@@ -11,11 +9,11 @@ public class Principal {
         // alt+enter em calcularAreaQuadra, para adicionar o import (linha 4)
         // antes - CalculadoraArea.calcularAreaQuadrado
         double areaQuadrado = calcularAreaQuadrado(5.2);
-        double areaCirculo =  CalculadoraArea.calcularAreaCirculo(10.5);
+        double areaCirculo =  calcularAreaCirculo(10.5);
 
-        out.printf("PI: %.2f%n",  CalculadoraArea.PI);
-        out.printf("Área do quadrado: %.2f%n", areaQuadrado);
-        out.printf("Área do círculo: %.2f%n", areaCirculo);
+        System.out.printf("PI: %.2f%n",  PI);
+        System.out.printf("Área do quadrado: %.2f%n", areaQuadrado);
+        System.out.printf("Área do círculo: %.2f%n", areaCirculo);
     }
 
 }
