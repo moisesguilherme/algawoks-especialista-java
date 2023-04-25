b1cdd7ee0800c305df9bd68c9da05e11b8a5fa64
Moisés Guilherme <moisescomp@gmail.com>
2023-04-25 20:25:29 -0300

    10.16. Múltiplas classes não-públicas em um único arquivo - depois

--- 10. Construtores, pacotes e visibilidade/10.16. Múltiplas classes não-públicas em um único arquivo/multiplas-classes/src/com/algaworks/erp/estoque/Produto.java
+++ 10. Construtores, pacotes e visibilidade/10.16. Múltiplas classes não-públicas em um único arquivo/multiplas-classes/src/com/algaworks/erp/estoque/Produto.java
@@ -1,14 +1,17 @@
 package com.algaworks.erp.estoque;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public class Produto {
 
     static final int QUANTIDADE_ESTOQUE_INICIAL = 100;
 
-    private final String codigo = "AAA";
+    private final String codigo;
     private String nome;
     private int quantidadeEstoque;
+    private Fornecedor fornecedor;
+    private Categoria categoria;
 
     public Produto() {
         this("Sem nome");
@@ -19,11 +22,44 @@ public class Produto {
     }
 
     private Produto(String nome, int estoqueInicial) {
+        Objects.requireNonNull(nome, "Nome é obrigatório");
+
+        if (estoqueInicial < 0) {
+            throw new IllegalArgumentException("Estoque inicial não pode ser negativo");
+        }
+
+        this.nome = nome;
+        this.quantidadeEstoque = estoqueInicial;
+        this.codigo = gerarCodigo();
     }
 
     private String gerarCodigo() {
         return UUID.randomUUID().toString();
     }
 
+    public void imprimir() {
+        System.out.printf("%s x %d itens", nome, quantidadeEstoque);
+    }
 
+
+}
+// Não pode ter 2 classes públicas no mesmo arquivo
+/*public class Fornecedor {
+
+}
+*/
+// A private funciona, vai aparecer na IDE como se tivesse um arquivo
+class Fornecedor {
+
+}
+
+class Categoria {
+
+}
+
+class Principal {
+    public static void main(String[] args) {
+        Produto produto = new Produto("Suco de uva");
+        produto.imprimir();
+    }
 }
