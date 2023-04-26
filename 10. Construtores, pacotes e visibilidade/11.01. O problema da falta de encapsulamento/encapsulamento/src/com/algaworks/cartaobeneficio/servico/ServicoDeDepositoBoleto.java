b1bf34ba3f0ceff0cdf3da28dc5caddec0f7d6ce
Moisés Guilherme <moisescomp@gmail.com>
2023-04-25 22:03:34 -0300

    11.1. O problema da falta de encapsulamento - citando os problemas

---
+++ 10. Construtores, pacotes e visibilidade/11.01. O problema da falta de encapsulamento/encapsulamento/src/com/algaworks/cartaobeneficio/servico/ServicoDeDepositoBoleto.java
@@ -0,0 +1,26 @@
+package com.algaworks.cartaobeneficio.servico;
+
+import com.algaworks.cartaobeneficio.Cartao;
+import com.algaworks.cartaobeneficio.Recibo;
+
+public class ServicoDeDepositoBoleto {
+
+    public Recibo efetuarDeposito(Cartao cartao, double valorDeposito) {
+        // TODO faz cobranca do valor no Boleto.
+
+        // Quando vai adicionando regra de negócio fica difícil
+        // alterar pois fica duplicado.
+        if(valorDeposito < Cartao.VALOR_MINIMO_DEPOSITO) {
+            throw new IllegalArgumentException(
+                    // Uso do String.format é igual ao printf(%s %.2f)
+                    String.format("Valor de depósito não pode ser menor ue %.2f", Cartao.VALOR_MINIMO_DEPOSITO)
+            );
+        }
+
+        cartao.saldo += valorDeposito - Cartao.TARIFA_DEPOSITO;
+
+        return new Recibo(cartao.titular, "Depósito", valorDeposito);
+
+    }
+
+}
