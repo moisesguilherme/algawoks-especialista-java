f41d8bd9d72e7b55d200882e8fe3bf7d6ca503d5
Moisés Guilherme <moisescomp@gmail.com>
2023-05-01 17:03:53 -0300

    11.6. Boas práticas: por que usar méodos de acesso

--- 10. Construtores, pacotes e visibilidade/11.06. Boas práticas - use métodos de acesso em classes públicas (incluindo Tell Don't Ask)/agenda/src/com/algaworks/agenda/Horario.java
+++ 10. Construtores, pacotes e visibilidade/11.06. Boas práticas - use métodos de acesso em classes públicas (incluindo Tell Don't Ask)/agenda/src/com/algaworks/agenda/Horario.java
@@ -2,10 +2,20 @@ package com.algaworks.agenda;
 
 public class Horario {
 
-    public int hora;
-    public int minuto;
+    // colocar final vai proteger, mas tem um problema, as classes que esteja usando
+    // externas vai precisar mudar
+    //public final int hora;
+    //public final int minuto;
 
+    // trocando para array
+    // problema dessa forma: vc não encapsulou e comeca a expor muito sua classe
+    // e fica difícil alterar internamente. Vai quebrar o código q chama externametne.
+    public int[] valores = new int[2];
     // Ideias do livro: Effect Java - Java Efetivo Joshua Bloch
+    // sempre criar métodos de acessos em classes públicas, os métodos de acesso
+    // getters e setters
+
+
     public Horario(int hora, int minuto) {
         if (hora < 0 || hora > 23) {
             throw new IllegalArgumentException("Hora inválida: " + hora);
@@ -15,8 +25,10 @@ public class Horario {
             throw new IllegalArgumentException("Minuto inválido: " + minuto);
         }
 
-        this.hora = hora;
-        this.minuto = minuto;
+        //this.hora = hora;
+        //this.minuto = minuto;
+        this.valores[0] = hora;
+        this.valores[1] = minuto;
 
     }
 
--- 10. Construtores, pacotes e visibilidade/11.06. Boas práticas - use métodos de acesso em classes públicas (incluindo Tell Don't Ask)/agenda/src/com/algaworks/agenda/Principal.java
+++ 10. Construtores, pacotes e visibilidade/11.06. Boas práticas - use métodos de acesso em classes públicas (incluindo Tell Don't Ask)/agenda/src/com/algaworks/agenda/Principal.java
@@ -4,6 +4,8 @@ public class Principal {
 
     public static void main(String[] args) {
         Horario horario = new Horario(10, 55);
+        // Não consegue adicionar pois é final
+        // horario.hora = 20;
         System.out.printf("%dh%dm", horario.hora, horario.minuto);
 
 
