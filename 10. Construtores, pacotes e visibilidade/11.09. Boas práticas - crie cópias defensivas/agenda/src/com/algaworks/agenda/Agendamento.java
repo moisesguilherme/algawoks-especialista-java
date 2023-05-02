8294e0ab4ab149f710b2f8fa5366dc07fb5642f9
Moisés Guilherme <moisescomp@gmail.com>
2023-05-01 21:34:24 -0300

    11.9. Boas práticas: crie cópias defensivas - antes

---
+++ 10. Construtores, pacotes e visibilidade/11.09. Boas práticas - crie cópias defensivas/agenda/src/com/algaworks/agenda/Agendamento.java
@@ -0,0 +1,29 @@
+package com.algaworks.agenda;
+
+public class Agendamento {
+
+    private final Horario horario;
+    private String descricao;
+
+    public Agendamento(Horario horario, String descricao) {
+        this.horario = horario;
+        this.descricao = descricao;
+    }
+
+    public Horario getHorario() {
+        return horario;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getHorarioFormatado() {
+        return horario.formatar();
+    }
+
+}
---
+++ 10. Construtores, pacotes e visibilidade/11.09. Boas práticas - crie cópias defensivas/agenda/src/com/algaworks/agenda/Principal.java
@@ -0,0 +1,18 @@
+package com.algaworks.agenda;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Horario horario = new Horario(10, 30);
+        Agendamento agendamentoCabelo = new Agendamento(horario, "Corte de cabelo");
+        Agendamento agendamentoBarba = new Agendamento(horario, "Feitio de barba");
+
+        imprimir(agendamentoCabelo);
+        imprimir(agendamentoBarba);
+    }
+
+    private static void imprimir(Agendamento agendamento) {
+        System.out.printf("%s às %s%n", agendamento.getDescricao(), agendamento.getHorarioFormatado());
+    }
+
+}
