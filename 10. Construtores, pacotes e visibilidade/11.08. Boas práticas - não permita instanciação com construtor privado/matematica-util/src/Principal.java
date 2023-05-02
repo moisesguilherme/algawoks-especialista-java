a9cf30bdd78823a344eeea11bdae72c37c095c6e
Moisés Guilherme <moisescomp@gmail.com>
2023-05-01 21:27:30 -0300

    11.8. Boas práticas: não permita instanciação com construtor privado - anotações

--- 10. Construtores, pacotes e visibilidade/11.08. Boas práticas - não permita instanciação com construtor privado/matematica-util/src/Principal.java
+++ 10. Construtores, pacotes e visibilidade/11.08. Boas práticas - não permita instanciação com construtor privado/matematica-util/src/Principal.java
@@ -4,6 +4,12 @@ public class Principal {
 
     public static void main(String[] args) {
 
+
+        // É possível instanciar essa classe
+        // CalculadoraArea calculadoraArea = new CalculadoraArea();
+        // double PI = calculadoraArea.PI;
+        // Para evitar isso, precisamos restringir o construtor da classe
+
         double areaQuadrado = CalculadoraArea.calcularAreaQuadrado(5.2);
         double areaCirculo = CalculadoraArea.calcularAreaCirculo(10.5);
 
--- 10. Construtores, pacotes e visibilidade/11.08. Boas práticas - não permita instanciação com construtor privado/matematica-util/src/com/algaworks/matematica/CalculadoraArea.java
+++ 10. Construtores, pacotes e visibilidade/11.08. Boas práticas - não permita instanciação com construtor privado/matematica-util/src/com/algaworks/matematica/CalculadoraArea.java
@@ -3,6 +3,12 @@ package com.algaworks.matematica;
 
 public class CalculadoraArea {
 
+    // Quando não tem nehum construtor, é o padrão sem argumento
+    // Não vai conseguir instanciar um objeto
+    private CalculadoraArea() {
+
+    }
+
     public static final double PI = 3.14159265358979323846;
     public static double calcularAreaQuadrado(double medidaDoLado) {
         return medidaDoLado * medidaDoLado;
