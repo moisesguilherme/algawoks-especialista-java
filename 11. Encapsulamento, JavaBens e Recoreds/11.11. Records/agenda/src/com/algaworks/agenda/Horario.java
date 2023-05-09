0f5e0921140d20fe11bda7377af8b440e22ab63f
Moisés Guilherme <moisescomp@gmail.com>
2023-05-08 21:41:21 -0300

    11.11. Records

---
+++ 11. Encapsulamento, JavaBens e Recoreds/11.11. Records/agenda/src/com/algaworks/agenda/Horario.java
@@ -0,0 +1,37 @@
+package com.algaworks.agenda;
+
+// Códigos boilerplate, muito setters e getters
+// Java 14 e lancado no java 16 é o record
+public final class Horario {
+    // vai remover tudo
+    /*
+    private final int hora;
+    private final int minuto;
+
+    public Horario(int hora, int minuto) {
+
+        if (hora < 0 || hora > 23) {
+            throw new IllegalArgumentException("Hora inválida: " + hora);
+        }
+
+        if (minuto < 0 || minuto > 59) {
+            throw new IllegalArgumentException("Minuto inválido: " + minuto);
+        }
+
+        this.hora = hora;
+        this.minuto = minuto;
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public int getMinuto() {
+        return minuto;
+    }
+
+    public String formatar() {
+        return String.format("%dh%dm", getHora(), getMinuto());
+    }*/
+
+}
