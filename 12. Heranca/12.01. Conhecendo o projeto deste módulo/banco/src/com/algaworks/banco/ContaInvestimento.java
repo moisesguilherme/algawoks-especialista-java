5afefabcf8a6e9b587073a2a2daa772c5431a5f4
Moisés Guilherme <moisescomp@gmail.com>
2023-05-21 19:34:29 -0300

    12.5. Implementando herança

--- 12. Heranca/12.01. Conhecendo o projeto deste módulo/banco/src/com/algaworks/banco/ContaInvestimento.java
+++ 12. Heranca/12.01. Conhecendo o projeto deste módulo/banco/src/com/algaworks/banco/ContaInvestimento.java
@@ -1,45 +1,8 @@
 package com.algaworks.banco;
 
-public class ContaInvestimento {
-
-    private Titular titular;
-    private int agencia;
-    private int numero;
-    private double saldo;
+public class ContaInvestimento extends Conta{
 
     private double valorTotalRendimento;
-
-    public Titular getTitular() {
-        return titular;
-    }
-
-    public void setTitular(Titular titular) {
-        this.titular = titular;
-    }
-
-    public int getAgencia() {
-        return agencia;
-    }
-
-    public void setAgencia(int agencia) {
-        this.agencia = agencia;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
-
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
-    }
 
     public double getValorTotalRendimento() {
         return valorTotalRendimento;
@@ -49,34 +12,6 @@ public class ContaInvestimento {
         double valorRendimentos = getSaldo() * percentualJuros / 100;
         this.valorTotalRendimento += valorRendimentos;
         depositar(valorRendimentos);
-    }
-
-    public void sacar(double valorSaque) {
-        if (valorSaque <= 0) {
-            throw new IllegalArgumentException("Valor do saque deve ser maior que 0");
-        }
-
-        if (getSaldo() < valorSaque) {
-            throw new RuntimeException("Saldo insuficiente para saque");
-        }
-
-        saldo -= valorSaque;
-    }
-
-    public void depositar(double valorDeposito) {
-        if (valorDeposito <= 0) {
-            throw new IllegalArgumentException("Valor do depósito deve ser maior que 0");
-        }
-
-        saldo += valorDeposito;
-    }
-
-    public void imprimirDemonstrativo() {
-        System.out.println();
-        System.out.printf("Agência: %d%n", getAgencia());
-        System.out.printf("Conta: %d%n", getNumero());
-        System.out.printf("Titular: %s%n", getTitular().getNome());
-        System.out.printf("Saldo: %.2f%n", getSaldo());
     }
 
 }
