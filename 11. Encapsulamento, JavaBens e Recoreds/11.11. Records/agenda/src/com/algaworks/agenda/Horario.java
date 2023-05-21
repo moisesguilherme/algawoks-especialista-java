0475cc6e37415096896a277f9c8da99c72e64ee7
Moisés Guilherme <moisescomp@gmail.com>
2023-05-21 16:25:20 -0300

    11.11. Records - getters

--- 11. Encapsulamento, JavaBens e Recoreds/11.11. Records/agenda/src/com/algaworks/agenda/Horario.java
+++ 11. Encapsulamento, JavaBens e Recoreds/11.11. Records/agenda/src/com/algaworks/agenda/Horario.java
@@ -2,36 +2,44 @@ package com.algaworks.agenda;
 
 // Códigos boilerplate, muito setters e getters
 // Java 14 e lancado no java 16 é o record
-public final class Horario {
-    // vai remover tudo
-    /*
-    private final int hora;
-    private final int minuto;
+// toda classe record já é final, é uma classe imutável
+public record Horario(int hora, int minuto) {
+    // define a lista de propriedades: int hora, int minuto
+    // o compilador cria automaticamente as variáveis de instância
+    // private int hora;
+    // private int minuto
+    // também cria um construtor
 
-    public Horario(int hora, int minuto) {
+    //Cria de uma forma implícita, não pode alterar a ordem do construtor
+    /*public Horario(int hora, int minuto) {
+         if (hora < 0 || hora > 23) {
+             throw new IllegalArgumentException("Hora inválida: " + hora);
+         }
+         if(minuto < 0 || minuto > 59) {
+             throw new IllegalArgumentException("Minuto inválido: " + minuto);
+         }
 
+         this.hora = hora;
+         this.minuto = minuto;
+     }*/
+
+    //Compact constructor, sem parâmetros
+    public Horario {
         if (hora < 0 || hora > 23) {
             throw new IllegalArgumentException("Hora inválida: " + hora);
         }
-
-        if (minuto < 0 || minuto > 59) {
+        if(minuto < 0 || minuto > 59) {
             throw new IllegalArgumentException("Minuto inválido: " + minuto);
         }
 
-        this.hora = hora;
-        this.minuto = minuto;
+        //sem this, ele coloca automáticamente
+        //this.hora = hora;
+        //this.minuto = minuto;
     }
 
-    public int getHora() {
-        return hora;
-    }
-
-    public int getMinuto() {
-        return minuto;
-    }
 
     public String formatar() {
-        return String.format("%dh%dm", getHora(), getMinuto());
-    }*/
+        return String.format("%dh%dm", hora, minuto); //ou hora(), minuto()
+    }
 
 }
--- 11. Encapsulamento, JavaBens e Recoreds/11.11. Records/agenda/src/com/algaworks/agenda/Principal.java
+++ 11. Encapsulamento, JavaBens e Recoreds/11.11. Records/agenda/src/com/algaworks/agenda/Principal.java
@@ -4,6 +4,9 @@ public class Principal {
 
     public static void main(String[] args) {
         Horario horario = new Horario(10, 30);
+        System.out.println(horario.hora()); // tem acesso ao getter
+        // Não existe setter, a classe é imutável
+        // Não pode alterar o estado, ele é final.
 
         Agendamento agendamentoCabelo = new Agendamento(horario, "Corte de cabelo");
         agendamentoCabelo.setHorario(new Horario(16, 20));
