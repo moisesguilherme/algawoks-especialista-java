5be6a04a78244a42308b71e6fa150886e0a1ba6a
Moisés Guilherme <moisescomp@gmail.com>
2023-06-04 18:03:40 -0300

    14.6. Quando herança de classes se torna um problema

---
+++ 14. Interfaces/14.05. Conhecendo o projeto da financeira/financeira/src/com/algaworks/financeira/modelo/ParceiroFinanceiro.java
@@ -0,0 +1,24 @@
+package com.algaworks.financeira.modelo;
+
+public class ParceiroFinanceiro extends Empresa {
+
+    private double valorTotalAplicado;
+
+    public ParceiroFinanceiro(String razaoSocial, double totalFaturamento) {
+        super(razaoSocial, totalFaturamento);
+    }
+
+    public double getValorTotalAplicado() {
+        return valorTotalAplicado;
+    }
+
+    public void adicionarNovaAplicacao(double valorAplicado) {
+        valorTotalAplicado += valorAplicado;
+    }
+
+    @Override
+    public double calcularLimiteAprovado() {
+        throw new RuntimeException("Não é possível financiar um parceiro");
+    }
+
+}
