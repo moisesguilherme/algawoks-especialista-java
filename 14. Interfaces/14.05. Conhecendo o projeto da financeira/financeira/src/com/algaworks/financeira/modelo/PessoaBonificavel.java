92de77506f67128930bac44e7d9617ff0191c43f
Moisés Guilherme <moisescomp@gmail.com>
2023-06-05 08:04:54 -0300

    14.13. Implementando múltiplas interfaces - problema

--- 14. Interfaces/14.05. Conhecendo o projeto da financeira/financeira/src/com/algaworks/financeira/modelo/PessoaBonificavel.java
+++ 14. Interfaces/14.05. Conhecendo o projeto da financeira/financeira/src/com/algaworks/financeira/modelo/PessoaBonificavel.java
@@ -3,4 +3,11 @@ package com.algaworks.financeira.modelo;
 public interface PessoaBonificavel {
 
     double calcularBonus(double percentualMetaAlcancada);
+
+    // Problema: A classe Funcionário implementa a interface ClienteFinanciavel
+    // e a classe PessoaBonificavel. Esse método está nas duas interfaces.
+    // Neste casso vai dar um erro. (Só um exemplo, essa interface não vai ter esse método)
+    /*default double calcularJuros(double valorSolicitado) {
+        return 0;
+    }*/
 }
