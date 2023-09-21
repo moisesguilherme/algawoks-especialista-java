7a8b9a2c7ccf6be84d28ec2909841f1847224859
Moisés Guilherme <moisescomp@gmail.com>
2023-09-21 07:59:35 -0300

    9.1. Estrutura da memória da JVM

---
+++ 9. Gerenciamento de memoria da JVM/09.03. Informações da Memória Heap com a Runtime API/testes-jvm/src/Teste.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Teste {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o seu nome:");
+        String nome = scanner.nextLine();
+
+        System.out.println("Olá, " + nome);
+
+    }
+
+}
