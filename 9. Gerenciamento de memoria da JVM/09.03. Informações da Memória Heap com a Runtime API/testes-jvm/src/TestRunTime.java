2c5b6612e89f8f5a6254c24f0c7e41e84027fa32
Moisés Guilherme <moisescomp@gmail.com>
2023-09-22 08:29:53 -0300

    9.3. Informações da Memória Heap com a Runtime API

---
+++ 9. Gerenciamento de memoria da JVM/09.03. Informações da Memória Heap com a Runtime API/testes-jvm/src/TestRunTime.java
@@ -0,0 +1,34 @@
+public class TestRunTime {
+
+    public static void main(String[] args) {
+        imprimirUsoMemoria();
+
+        byte[] x = new byte[500 * 1024 * 1024];
+        byte[] y = new byte[500 * 1024 * 1024];
+
+        imprimirUsoMemoria();
+
+    }
+
+    static void imprimirUsoMemoria() {
+        // Memória q a JVM pode usar
+        System.out.printf("Máxima: %s%n", emMegabytes(Runtime.getRuntime().maxMemory()));
+
+        // Total de memória reservada
+        System.out.printf("Total empenhada: %s%n", emMegabytes(Runtime.getRuntime().totalMemory()));
+
+        // Total de memória disponível
+        System.out.printf("Disponível: %s%n", emMegabytes(Runtime.getRuntime().freeMemory()));
+
+        long memoriaUsada = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+
+        System.out.printf("Usada: %s%n", emMegabytes(memoriaUsada));
+
+        System.out.println("----");
+    }
+
+
+    static String emMegabytes(long bytes) {
+        return String.format("%.2fMB", bytes / 1024d / 1024d);
+    }
+}
