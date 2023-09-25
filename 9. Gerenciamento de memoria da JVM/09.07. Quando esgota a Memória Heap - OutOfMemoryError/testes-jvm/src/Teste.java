8db8160f4b9fe757dcd1ae0a50e553dd8c3d5ece
Moisés Guilherme <moisescomp@gmail.com>
2023-09-24 21:17:17 -0300

    9.7. Quando esgota a Memória Heap: OutOfMemoryError

---
+++ 9. Gerenciamento de memoria da JVM/09.07. Quando esgota a Memória Heap - OutOfMemoryError/testes-jvm/src/Teste.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class Teste {
+
+    // Colocando argumento na JVM
+    // -Xms4G -Xmx4G
+    public static void main(String[] args) {
+        ArrayList<Cliente> clientes = new ArrayList<>();
+
+        while (true) {
+            imprimirUsoMemoria();
+            clientes.add(new Cliente());
+        }
+    }
+
+    static void imprimirUsoMemoria() {
+        System.out.printf("Máxima: %s%n",
+                emMegabytes(Runtime.getRuntime().maxMemory()));
+
+        System.out.printf("Total empenhada: %s%n",
+                emMegabytes(Runtime.getRuntime().totalMemory()));
+
+        System.out.printf("Disponível: %s%n",
+                emMegabytes(Runtime.getRuntime().freeMemory()));
+
+        long memoriaUsada = Runtime.getRuntime().totalMemory()
+                - Runtime.getRuntime().freeMemory();
+        System.out.printf("Usada: %s%n", emMegabytes(memoriaUsada));
+
+        System.out.println("---");
+    }
+
+    static String emMegabytes(long bytes) {
+        return String.format("%.2fMB", bytes / 1024d / 1024d);
+    }
+
+}
