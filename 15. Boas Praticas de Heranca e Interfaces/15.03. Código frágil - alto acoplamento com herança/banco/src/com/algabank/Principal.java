58c3eefc95ed73e4b0a28438ed9a92ac4a1acf40
Moisés Guilherme <moisescomp@gmail.com>
2023-09-26 06:40:38 -0300

    15.3. Código frágil: alto acoplamento com herança

--- 15. Boas Praticas de Heranca e Interfaces/15.03. Código frágil - alto acoplamento com herança/banco/src/com/algabank/Principal.java
+++ 15. Boas Praticas de Heranca e Interfaces/15.03. Código frágil - alto acoplamento com herança/banco/src/com/algabank/Principal.java
@@ -10,8 +10,9 @@ public class Principal {
         Conta conta2 = new ContaCorrente();
 
         conta1.depositar(1000);
-        conta1.sacar(100);
+        // conta1.sacar(100);
         conta1.transferir(conta2, 100);
+        //conta1.aplicarEmInvestimento(100);
 
         System.out.printf("Saldo da conta 1: R$%.2f%n", conta1.getSaldo());
         System.out.printf("Saldo da conta 2: R$%.2f%n", conta2.getSaldo());
--- 15. Boas Praticas de Heranca e Interfaces/15.03. Código frágil - alto acoplamento com herança/banco/src/com/javabank/ContaCorrente.java
+++ 15. Boas Praticas de Heranca e Interfaces/15.03. Código frágil - alto acoplamento com herança/banco/src/com/javabank/ContaCorrente.java
@@ -11,6 +11,7 @@ public class ContaCorrente implements Conta {
 
     @Override
     public void sacar(double valor) {
+        System.out.println(">>> chamou sacar super classe");
         this.saldo -= valor;
     }
 
@@ -22,12 +23,14 @@ public class ContaCorrente implements Conta {
     @Override
     public void transferir(Conta conta, double valor) {
         //this.saldo -= valor;
-        sacar(valor);
+        sacar(valor); //Problema
+        sacarTarifa(15); // vai chamar a do ContaCorrenteComTribuicao
         conta.depositar(valor);
     }
 
     @Override
     public void aplicarEmInvestimento(double valor) {
+        System.out.println(">>> chamou sacar super aplicarEmInvestimento");
         this.saldo -= valor;
         // TODO aqui aplicaria o dinheiro no investimento
     }
