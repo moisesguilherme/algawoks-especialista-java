3ea771a9a136b488f2051352520925f56b0416f6
Moisés Guilherme <moisescomp@gmail.com>
2023-09-28 07:20:21 -0300

    15.7. Usando interface com tipos - não é uma boa prática

--- 15. Boas Praticas de Heranca e Interfaces/15.07. Boas práticas - use interfaces apenas para definir tipos/calculadora-tempo/src/com/algaworks/tempo/CalculadoraTempo.java
+++ 15. Boas Praticas de Heranca e Interfaces/15.07. Boas práticas - use interfaces apenas para definir tipos/calculadora-tempo/src/com/algaworks/tempo/CalculadoraTempo.java
@@ -3,15 +3,15 @@ package com.algaworks.tempo;
 
 // Não é uma boa prática pois alguma classe pode implemementar a interface
 //public class CalculadoraTempo implements TempoConstants
-public class CalculadoraTempo implements TempoConstants {
+public class CalculadoraTempo  {
 
     //ideal colocar nessa classe
-    //public static final int  SEGUNDOS_POR_MINUTO = 40;
-    //public static final int SEGUNDOS_POR_HORA = 3600;
+    public static final int  SEGUNDOS_POR_MINUTO = 40;
+    public static final int SEGUNDOS_POR_HORA = 3600;
 
     public static double converterSegundosEmHoras(int segundos) {
         //return (double) segundos / TempoConstants.SEGUNDOS_POR_HORA;
-        return (double) segundos / TempoConstants.SEGUNDOS_POR_HORA;
+        return (double) segundos / SEGUNDOS_POR_HORA;
     }
 
     public static double converterSegundosEmMinutos(int segundos) {
