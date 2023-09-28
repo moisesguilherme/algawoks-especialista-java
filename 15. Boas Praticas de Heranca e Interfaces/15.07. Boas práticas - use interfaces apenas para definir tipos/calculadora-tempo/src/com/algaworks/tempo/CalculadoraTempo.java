d49083fd6ca10a921729b343c8f0795989ddd7c1
Moisés Guilherme <moisescomp@gmail.com>
2023-09-28 07:25:56 -0300

    15.7. Resolvendo usando tipos

--- 15. Boas Praticas de Heranca e Interfaces/15.07. Boas práticas - use interfaces apenas para definir tipos/calculadora-tempo/src/com/algaworks/tempo/CalculadoraTempo.java
+++ 15. Boas Praticas de Heranca e Interfaces/15.07. Boas práticas - use interfaces apenas para definir tipos/calculadora-tempo/src/com/algaworks/tempo/CalculadoraTempo.java
@@ -5,18 +5,13 @@ package com.algaworks.tempo;
 //public class CalculadoraTempo implements TempoConstants
 public class CalculadoraTempo  {
 
-    //ideal colocar nessa classe
-    public static final int  SEGUNDOS_POR_MINUTO = 40;
-    public static final int SEGUNDOS_POR_HORA = 3600;
 
     public static double converterSegundosEmHoras(int segundos) {
-        //return (double) segundos / TempoConstants.SEGUNDOS_POR_HORA;
-        return (double) segundos / SEGUNDOS_POR_HORA;
+        return (double) segundos / TempoConstants.SEGUNDOS_POR_HORA;
     }
 
     public static double converterSegundosEmMinutos(int segundos) {
-        //return (double) segundos / TempoConstants.SEGUNDOS_POR_MINUTO;
-        return (double) segundos / SEGUNDOS_POR_MINUTO;
+        return (double) segundos / TempoConstants.SEGUNDOS_POR_MINUTO;
     }
 
 }
--- 15. Boas Praticas de Heranca e Interfaces/15.07. Boas práticas - use interfaces apenas para definir tipos/calculadora-tempo/src/com/algaworks/tempo/TempoConstants.java
+++ 15. Boas Praticas de Heranca e Interfaces/15.07. Boas práticas - use interfaces apenas para definir tipos/calculadora-tempo/src/com/algaworks/tempo/TempoConstants.java
@@ -1,14 +1,15 @@
 package com.algaworks.tempo;
 
-//
-public interface TempoConstants {
-
-    // Interface de constantes, não é uma boa prática
-
-    // uma variável int aqui é public static final.
-    // final int SEGUNDOS_POR_MINUTO = 60;
+// Criando uma classe utilitária
+public final class TempoConstants {
 
     public static final int SEGUNDOS_POR_MINUTO = 60;
     public static final int SEGUNDOS_POR_HORA = 3600;
+
+
+    // Criar um construtor privado para não instanciar a classe
+    private TempoConstants() {
+
+    }
 
 }
