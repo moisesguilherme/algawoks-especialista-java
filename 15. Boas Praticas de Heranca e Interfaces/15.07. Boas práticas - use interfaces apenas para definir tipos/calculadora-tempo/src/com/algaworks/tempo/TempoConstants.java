3bed39c87f4679e738d899f42d2373dfc0428c95
Moisés Guilherme <moisescomp@gmail.com>
2023-09-28 07:11:21 -0300

    15.7. Boas práticas: use interfaces apenas para definir tipos

---
+++ 15. Boas Praticas de Heranca e Interfaces/15.07. Boas práticas - use interfaces apenas para definir tipos/calculadora-tempo/src/com/algaworks/tempo/TempoConstants.java
@@ -0,0 +1,13 @@
+package com.algaworks.tempo;
+
+public final class TempoConstants {
+
+    // Interface de constantes, não é uma boa prática
+
+    // uma variável int aqui é public static final.
+    // final int SEGUNDOS_POR_MINUTO = 60;
+
+    //public static final int SEGUNDOS_POR_MINUTO = 60;
+    //public static final int SEGUNDOS_POR_HORA = 3600;
+
+}
