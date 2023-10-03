472b4b628499333a57a544cae03bd8b998034add
Moisés Guilherme <moisescomp@gmail.com>
2023-10-03 08:14:07 -0300

    16.8. Capturando checked exceptions

---
+++ 16. Introducao as excecoes/16.08 Capturando checked exceptions/excecoes/src/Principal.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Principal {
+    public static void main(String[] args) {
+        Path arquivo = Path.of("/home/moises/Desktop/DEV/AlgaWorks/EspecialistaJava/teste.txt");
+
+        try {
+            Files.createFile(arquivo);
+        } catch (IOException e) {
+            System.out.println("Erro ao criar arquivo: " + e.getMessage());
+        }
+
+    }
+}
