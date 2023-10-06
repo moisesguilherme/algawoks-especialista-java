f7ad2a2c0d9adb0b6d553aafa88ee5444dc70901
Moisés Guilherme <moisescomp@gmail.com>
2023-10-06 08:04:09 -0300

    16.16. Usando a cláusula finally - problema - pt1

--- 16. Introducao as excecoes/16.16. Usando a cláusula finally/excecoes/src/Principal.java
+++ 16. Introducao as excecoes/16.16. Usando a cláusula finally/excecoes/src/Principal.java
@@ -7,14 +7,22 @@ public class Principal {
 
     public static void main(String[] args) {
         Path arquivo = Path.of("/home/moises/Desktop/DEV/AlgaWorks/EspecialistaJava/teste2.txt");
+        BufferedReader reader = null;
 
         try {
-            BufferedReader reader = Files.newBufferedReader(arquivo);
+            reader = Files.newBufferedReader(arquivo);
             System.out.println(reader.readLine()); // pode lancar uma IOException
             // se for executado vai para o catch, e o reader.close() nao vai ser executado
-            reader.close();
+
         } catch (IOException e) {
             System.out.println("Erro ao ler arquivo: " + e.getMessage());
+
+            // Nào é uma boa prática.
+            try {
+                reader.close();
+            } catch (IOException ex) {
+                System.out.println("Erro fechando leitor de arquivo");;
+            }
         }
     }
 
