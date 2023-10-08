ff316d8692caf3cc87fc13c7b5c01339a6fa08e7
Moisés Guilherme <moisescomp@gmail.com>
2023-10-08 19:35:26 -0300

    16.19. Boas práticas: lance exceções ao invés de retornar null - depois

--- 16. Introducao as excecoes/16.19. Boas práticas - lance exceções ao invés de retornar null/excecoes/src/Principal.java
+++ 16. Introducao as excecoes/16.19. Boas práticas - lance exceções ao invés de retornar null/excecoes/src/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 
     public static void main(String[] args) {
         ServicoCadastroCliente servicoCadastroCliente = new ServicoCadastroCliente();
-        Cliente clienteCadastrado = servicoCadastroCliente.cadastrar("João", 18);
+        Cliente clienteCadastrado = servicoCadastroCliente.cadastrar("João", 15);
 
         System.out.printf("Cliente cadastrado: %s%n", clienteCadastrado.getNome());
     }
--- 16. Introducao as excecoes/16.19. Boas práticas - lance exceções ao invés de retornar null/excecoes/src/com/algaworks/crm/ServicoCadastroCliente.java
+++ 16. Introducao as excecoes/16.19. Boas práticas - lance exceções ao invés de retornar null/excecoes/src/com/algaworks/crm/ServicoCadastroCliente.java
@@ -4,7 +4,9 @@ public class ServicoCadastroCliente {
 
     public Cliente cadastrar(String nome, int idade) {
         if (idade < 18) {
-            return null;
+            // Violacão da regra de negócio...
+            //return null;
+            throw new MenorDeIdadeException("Cliente menor de idade não pode ser cadastrado");
         }
 
         // TODO faz o cadastro aqui
