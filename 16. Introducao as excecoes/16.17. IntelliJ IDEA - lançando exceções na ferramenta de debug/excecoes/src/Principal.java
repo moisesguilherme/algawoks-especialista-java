390d3e5de7547b8692129806802172a339ee8a87
Moisés Guilherme <moisescomp@gmail.com>
2023-10-08 19:08:42 -0300

    16.18. IntelliJ IDEA: adicionando Java Exception Breakpoints

--- 16. Introducao as excecoes/16.17. IntelliJ IDEA - lançando exceções na ferramenta de debug/excecoes/src/Principal.java
+++ 16. Introducao as excecoes/16.17. IntelliJ IDEA - lançando exceções na ferramenta de debug/excecoes/src/Principal.java
@@ -1,10 +1,13 @@
 public class Principal {
 
     public static void main(String[] args) {
+        int idade = 10;
+
         try {
-            // No Debug: Em main: btn direito e throw exception:
-            // new RuntimeException("Erro")
-            System.out.println("Teste");
+            // More em break-point (ctrl+shift+f8)
+            if(idade < 18) {
+                throw new IllegalArgumentException("Menor de idade");
+            }
         } catch (RuntimeException e) {
             System.out.println(e.getMessage());
         }
