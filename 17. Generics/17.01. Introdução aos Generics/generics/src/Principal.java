a744ba1dbabadf5433b846a48e82f8fda3948654
Moisés Guilherme <moisescomp@gmail.com>
2023-10-09 06:42:32 -0300

    17.1. Introdução aos Generics - part1

--- 17. Generics/17.01. Introdução aos Generics/generics/src/Principal.java
+++ 17. Generics/17.01. Introdução aos Generics/generics/src/Principal.java
@@ -5,22 +5,20 @@ import java.util.ArrayList;
 public class Principal {
 
     public static void main(String[] args) {
-        // ArrayList "puro" pode adicionar qualquer tipo de objeto
-        ArrayList clientes = new ArrayList();
-
+        // Declara ArrayList de <Cliente> e instancia ArrayList de <Cliente>
+        ArrayList<Cliente> clientes = new ArrayList<Cliente>();
         clientes.add(new Cliente("Supermercado Pague e Leve", 2_000_000));
         clientes.add(new Cliente("Posto Gasolina Boa", 800_000));
-        //Objeto de outro tipo (precisa do instanceof abaixo)
-        clientes.add("José");
+        //Tempo de execucão e tempo de compilacão.
+        //Com generics o código nem compila.
+        //clientes.add("José");
+
 
         double totalFaturamento = 0d;
+        //clientes.get(0);
 
-        for (Object objeto : clientes) {
-            Cliente cliente;
-            if(objeto instanceof Cliente) {
-                cliente = (Cliente) objeto;
-                totalFaturamento += cliente.getFaturamentoMensal();
-            }
+        for (Cliente cliente : clientes) {
+            totalFaturamento += cliente.getFaturamentoMensal();
         }
 
         System.out.println(totalFaturamento);
