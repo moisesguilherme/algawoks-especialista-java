03edb4b9aa5f038357d9a29988c10568bb321bff
Moisés Guilherme <moisescomp@gmail.com>
2023-10-09 06:48:05 -0300

    17.1. Introdução aos Generics - final

--- 17. Generics/17.01. Introdução aos Generics/generics/src/Principal.java
+++ 17. Generics/17.01. Introdução aos Generics/generics/src/Principal.java
@@ -5,8 +5,15 @@ import java.util.ArrayList;
 public class Principal {
 
     public static void main(String[] args) {
+
+        //Generics a partir do java 5, a partir do java 7 (a instancia não precisa especificar o tipo)
         // Declara ArrayList de <Cliente> e instancia ArrayList de <Cliente>
-        ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+        // operador diamante <> -> ArrayList<>()
+        // cuidado com operador var:
+        //var clientes = new ArrayList<Object>();
+        //var clientes = new ArrayList<Cliente>();
+
+        ArrayList<Cliente> clientes = new ArrayList<>();
         clientes.add(new Cliente("Supermercado Pague e Leve", 2_000_000));
         clientes.add(new Cliente("Posto Gasolina Boa", 800_000));
         //Tempo de execucão e tempo de compilacão.
