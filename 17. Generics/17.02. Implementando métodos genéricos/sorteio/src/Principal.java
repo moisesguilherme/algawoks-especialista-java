1b2c15db3c417dfbc6489899b70600c2a4b9662f
Moisés Guilherme <moisescomp@gmail.com>
2023-10-09 07:01:01 -0300

    17.2. Implementando métodos genéricos - antes

---
+++ 17. Generics/17.02. Implementando métodos genéricos/sorteio/src/Principal.java
@@ -0,0 +1,18 @@
+import com.algaworks.sorteio.Cliente;
+import com.algaworks.sorteio.Sorteador;
+
+public class Principal {
+    
+    public static void main(String[] args) {
+
+        Cliente[] clientes = {
+            new Cliente("Supermercado Pague e Leve", 2_000_000),
+            new Cliente("Posto Gasolina Boa", 800_000),
+            new Cliente("Javac Consultoria", 58_000_000)
+        };
+
+        Cliente clienteSorteado = Sorteador.sortear(clientes);
+        System.out.println(clienteSorteado.getRazaoSocial());
+    }
+
+}
