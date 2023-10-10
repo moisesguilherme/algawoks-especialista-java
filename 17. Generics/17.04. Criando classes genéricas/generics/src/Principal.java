49b60b535ffcfc9f972b4729b29300acb82ca208
Moisés Guilherme <moisescomp@gmail.com>
2023-10-10 07:11:32 -0300

    17.4. Criando classes genéricas - part2

--- 17. Generics/17.04. Criando classes genéricas/generics/src/Principal.java
+++ 17. Generics/17.04. Criando classes genéricas/generics/src/Principal.java
@@ -5,17 +5,20 @@ public class Principal {
 
     public static void main(String[] args) {
         Pilha pilha = new Pilha();
+        pilha.colocar("A");
+        pilha.colocar("B");
         pilha.colocar(new Produto("Arroz"));
         pilha.colocar(new Produto("Feijão"));
 
         Produto produto = (Produto) pilha.retirar();
         System.out.println(produto.getDescricao());
 
-        /*produto = (Produto) pilha.retirar();
+        produto = (Produto) pilha.retirar();
         System.out.println(produto.getDescricao());
 
+        // ClassCastException
         produto = (Produto) pilha.retirar();
-        System.out.println(produto.getDescricao());*/
+        System.out.println(produto.getDescricao());
 
 
     }
