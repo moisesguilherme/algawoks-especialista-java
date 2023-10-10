eec4c252adf24d474550447402b0d11b57e81fcb
Moisés Guilherme <moisescomp@gmail.com>
2023-10-10 07:35:57 -0300

    17.4. Criando classes genéricas - part3

--- 17. Generics/17.04. Criando classes genéricas/generics/src/Principal.java
+++ 17. Generics/17.04. Criando classes genéricas/generics/src/Principal.java
@@ -4,20 +4,21 @@ import com.algaworks.loja.Produto;
 public class Principal {
 
     public static void main(String[] args) {
-        Pilha pilha = new Pilha();
-        pilha.colocar("A");
-        pilha.colocar("B");
+        Pilha<Produto> pilha = new Pilha<Produto>();
+        //pilha.colocar("A");
+        //pilha.colocar("B");
         pilha.colocar(new Produto("Arroz"));
         pilha.colocar(new Produto("Feijão"));
+        pilha.colocar(new Produto("Macarrão"));
 
         Produto produto = (Produto) pilha.retirar();
         System.out.println(produto.getDescricao());
 
-        produto = (Produto) pilha.retirar();
+        produto = pilha.retirar();
         System.out.println(produto.getDescricao());
 
-        // ClassCastException
-        produto = (Produto) pilha.retirar();
+        // Não corre o risco de -> ClassCastException
+        produto = pilha.retirar();
         System.out.println(produto.getDescricao());
 
 
--- 17. Generics/17.04. Criando classes genéricas/generics/src/com/algaworks/estruturadedados/Pilha.java
+++ 17. Generics/17.04. Criando classes genéricas/generics/src/com/algaworks/estruturadedados/Pilha.java
@@ -2,26 +2,26 @@ package com.algaworks.estruturadedados;
 
 import java.util.Arrays;
 
-public class Pilha {
+public class Pilha<T> {
     //LIFO -> Last In First Out
 
-    private Object[] itens;
+    private T[] itens;
 
     public Pilha() {
-        itens =  new Object[0];
+        itens = (T[]) new Object[0];
     }
 
-    public void colocar(Object item) {
+    public void colocar(T item) {
         itens = Arrays.copyOf(itens, itens.length + 1);
         itens[itens.length - 1] = item;
     }
 
-    public Object retirar() {
+    public T retirar() {
         if (itens.length == 0) {
             throw new PilhaVaziaException("Pilha sem itens");
         }
 
-        Object item = itens[itens.length - 1];
+        T item = itens[itens.length - 1];
         itens = Arrays.copyOf(itens, itens.length - 1);
 
         return item;
