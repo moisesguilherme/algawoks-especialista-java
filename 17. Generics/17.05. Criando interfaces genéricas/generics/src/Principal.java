378ff5abc2abf68b1579cdae0e750dffc78496e4
Moisés Guilherme <moisescomp@gmail.com>
2023-10-11 07:47:52 -0300

    17.5. Criando interfaces genéricas - antes

---
+++ 17. Generics/17.05. Criando interfaces genéricas/generics/src/Principal.java
@@ -0,0 +1,29 @@
+import com.algaworks.estruturadedados.ColecaoVaziaException;
+import com.algaworks.estruturadedados.Pilha;
+import com.algaworks.loja.Produto;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Pilha<Produto> produtos = new Pilha<>();
+        produtos.colocar(new Produto("Arroz"));
+        produtos.colocar(new Produto("Feijão"));
+        produtos.colocar(new Produto("Água de coco"));
+
+        retirarTodos(produtos);
+    }
+
+    private static void retirarTodos(Pilha<Produto> produtos) {
+        try {
+            int i = 1;
+            while (true) {
+                Produto produto = produtos.retirar();
+                System.out.printf("%d. %s%n", i, produto.getDescricao());
+                i++;
+            }
+        } catch (ColecaoVaziaException e) {
+            System.out.println("Coleção de produtos esgotada");
+        }
+    }
+
+}
