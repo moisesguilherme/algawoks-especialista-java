814199bae9447eb65702b91c3388d6c3cc52c070
Moisés Guilherme <moisescomp@gmail.com>
2023-10-26 06:40:02 -0300

    18.30. Usando mapas dos tipos HashMap e Hashtable - part1

--- 18. Collections/18.30. Usando mapas dos tipos HashMap e Hashtable/collections-map/src/Principal.java
+++ 18. Collections/18.30. Usando mapas dos tipos HashMap e Hashtable/collections-map/src/Principal.java
@@ -16,7 +16,20 @@ public class Principal {
         Carro ford = new Carro("JAV5A67", "Ford Ka");
         Carro porsche = new Carro("EJA0V44", "Porsche 911");
 
-        
+        // Não pode colocar tipo primitivo
+        // Carro precisa ter equals e hashcode
+        Map<Carro, Proprietario> proprietarios = new HashMap<>();
+        proprietarios.put(bmw, maria);
+        proprietarios.put(honda, ana);
+        proprietarios.put(ford, jose);
+        proprietarios.put(porsche, maria);
+
+        //System.out.println(proprietarios); //toString em carro e proprietário
+
+        //System.out.println(proprietarios.get(ford));
+
+        // Pode ser por new, pois tem o equals e hashcode implementado
+        System.out.println(proprietarios.get(new Carro("JAV5A67", "Ford Ka 2")));
     }
 
 }
