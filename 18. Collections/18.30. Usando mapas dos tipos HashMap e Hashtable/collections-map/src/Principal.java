338f6dc6782d98926b915828e384d8653194564c
Moisés Guilherme <moisescomp@gmail.com>
2023-10-26 06:27:54 -0300

    18.30. Usando mapas dos tipos HashMap e Hashtable - início

---
+++ 18. Collections/18.30. Usando mapas dos tipos HashMap e Hashtable/collections-map/src/Principal.java
@@ -0,0 +1,22 @@
+import com.algaworks.detran.Carro;
+import com.algaworks.detran.Proprietario;
+
+import java.util.*;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Proprietario maria = new Proprietario("Maria", "05577788899");
+        Proprietario ana = new Proprietario("Ana", "33344488811");
+        Proprietario jose = new Proprietario("José", "09988877766");
+        Proprietario joao = new Proprietario("João", "22233344455");
+
+        Carro bmw = new Carro("ALG1A23", "BMW X3");
+        Carro honda = new Carro("WOR9K87", "Honda HR-V");
+        Carro ford = new Carro("JAV5A67", "Ford Ka");
+        Carro porsche = new Carro("EJA0V44", "Porsche 911");
+
+        
+    }
+
+}
