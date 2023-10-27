ca10bbdb4a21a9119d6bef3d55d12c0c332424e9
Moisés Guilherme <moisescomp@gmail.com>
2023-10-27 07:56:49 -0300

    18.33. Boas práticas: encapsulamento com coleções não-modificáveis - início

---
+++ 18. Collections/18.33. Boas práticas - encapsulamento com coleções não-modificáveis/encapsulamento-colecoes/src/Principal.java
@@ -0,0 +1,24 @@
+import com.algaworks.comercial.Cliente;
+import com.algaworks.comercial.ItemVenda;
+import com.algaworks.comercial.Venda;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        ItemVenda mouse = new ItemVenda("Magic mouse", 700);
+        ItemVenda adaptadorUsb = new ItemVenda("Adaptador USB", 400);
+        ItemVenda macbook = new ItemVenda("Macbook Pro", 15_000);
+        ItemVenda limpaTela = new ItemVenda("Limpa tela", 70);
+
+        Cliente cliente = new Cliente("João da Silva", 2_000);
+        Venda venda = new Venda(cliente);
+
+    }
+
+}
---
+++ 18. Collections/18.33. Boas práticas - encapsulamento com coleções não-modificáveis/encapsulamento-colecoes/src/com/algaworks/comercial/Venda.java
@@ -0,0 +1,32 @@
+package com.algaworks.comercial;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Venda {
+
+    private final Cliente cliente;
+    private List<ItemVenda> itens = new ArrayList<>();
+    private double valorTotal;
+
+    public Venda(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setItens(List<ItemVenda> itens) {
+        this.itens = itens;
+    }
+
+    public List<ItemVenda> getItens() {
+        return itens;
+    }
+}
