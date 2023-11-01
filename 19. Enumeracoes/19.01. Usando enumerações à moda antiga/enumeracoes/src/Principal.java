45bf279c9a93729f0a55517575af6ed5dc4d5e45
Moisés Guilherme <moisescomp@gmail.com>
2023-11-01 08:02:43 -0300

    19.8. Boas práticas: substitua parâmetros booleanos por enums - final

--- 19. Enumeracoes/19.01. Usando enumerações à moda antiga/enumeracoes/src/Principal.java
+++ 19. Enumeracoes/19.01. Usando enumerações à moda antiga/enumeracoes/src/Principal.java
@@ -7,8 +7,9 @@ public class Principal {
         pedido.setNomeCliente("João da Silva");
         pedido.setValorTotal(90);
 
-        servico.cancelar(pedido, true);
-
+        //servico.cancelar(pedido, true);
+        servico.cancelar(pedido, TipoUsuario.CLIENTE);
+        //servico.cancelar(pedido, null);
     }
 
 }
--- 19. Enumeracoes/19.01. Usando enumerações à moda antiga/enumeracoes/src/ServicoCancelamentoPedido.java
+++ 19. Enumeracoes/19.01. Usando enumerações à moda antiga/enumeracoes/src/ServicoCancelamentoPedido.java
@@ -1,9 +1,12 @@
 public class ServicoCancelamentoPedido {
 
-    public void cancelar(Pedido pedido, boolean cliente) {
+    public void cancelar(Pedido pedido, TipoUsuario tipoUsuario) {
         pedido.cancelar();
 
-        if (cliente) {
+        //boolean value = tipoUsuario.equals(TipoUsuario.CLIENTE); //Chance de NPE (NullPointerException)
+        boolean value = TipoUsuario.CLIENTE.equals(tipoUsuario); // Não ocorre o NPE
+
+        if (value) {
             System.out.println("Notificando gerente sobre cancelamento do pedido");
         }
 
