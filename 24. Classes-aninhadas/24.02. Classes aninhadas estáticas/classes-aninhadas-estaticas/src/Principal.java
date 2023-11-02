fc695f031427467cf3ebd5b8dacac1189896d80e
Moisés Guilherme <moisescomp@gmail.com>
2023-11-02 20:30:03 -0300

    24.2. Classes aninhadas estáticas - teste bizarro

--- 24. Classes-aninhadas/24.02. Classes aninhadas estáticas/classes-aninhadas-estaticas/src/Principal.java
+++ 24. Classes-aninhadas/24.02. Classes aninhadas estáticas/classes-aninhadas-estaticas/src/Principal.java
@@ -3,11 +3,15 @@ public class Principal {
     public static void main(String[] args) {
         ServicoEmail servico = new ServicoEmail();
 
-        Mensagem mensagem = new Mensagem(
+        ServicoEmail.testar();
+
+        ServicoEmail.Mensagem mensagem = new ServicoEmail.Mensagem(
                 "joao@algaworks.com", "maria@algaworks.com",
                 "Olá, tudo bem?");
 
         servico.enviar(mensagem);
+        ServicoEmail.Mensagem.testar();
+        ServicoEmail.Mensagem.Mensagem2.Mensagem3.teste3();
     }
 
 }
--- 24. Classes-aninhadas/24.02. Classes aninhadas estáticas/classes-aninhadas-estaticas/src/ServicoEmail.java
+++ 24. Classes-aninhadas/24.02. Classes aninhadas estáticas/classes-aninhadas-estaticas/src/ServicoEmail.java
@@ -4,4 +4,49 @@ public class ServicoEmail {
         System.out.printf("Enviando de %s para %s: %s%n", mensagem.getRemetente(), mensagem.getDestinatario(), mensagem.getTexto());
     }
 
+    public static void testar(){
+        System.out.println(">>> Método Estático Testar");
+    }
+
+
+    // Cola a classe Mensagem aqui
+    public static class Mensagem {
+
+        private final String remetente;
+        private final String destinatario;
+        private final String texto;
+
+        public static void testar(){
+            System.out.println(">>> Método estático testar da classe Mensagem");
+        }
+
+        public Mensagem(String remetente, String destinatario, String texto) {
+            this.remetente = remetente;
+            this.destinatario = destinatario;
+            this.texto = texto;
+        }
+
+        public String getRemetente() {
+            return remetente;
+        }
+
+        public String getDestinatario() {
+            return destinatario;
+        }
+
+        public String getTexto() {
+            return texto;
+        }
+
+        public static class Mensagem2 {
+
+            public static class Mensagem3 {
+                public static void teste3(){
+                    System.out.println(">>> Método estático testar da classe Mensagem 3");
+                }
+            }
+        }
+
+    }
+
 }
