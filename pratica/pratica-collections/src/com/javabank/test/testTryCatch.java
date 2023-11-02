8b455addab8afe1b0afbe8635a9fae0029d5ee88
Moisés Guilherme <moisescomp@gmail.com>
2023-11-01 21:28:56 -0300

    Using try and catch and multiple catch

---
+++ pratica/pratica-collections/src/com/javabank/test/testTryCatch.java
@@ -0,0 +1,26 @@
+package com.javabank.test;
+
+import com.javabank.account.Account;
+
+public class testTryCatch {
+
+    public static void main(String[] args) {
+
+        Account c1 = new Account();
+        String name;
+        Integer account;
+
+        name = c1.getAccountName();
+        c1.setAccountNumber(null); //NPE -> Other
+
+        try {
+            account = Integer.parseInt(c1.getAccountNumber()); // NumberFormatException
+            //account = Integer.parseInt(c1.getAccountNumber().toString()); // NPE
+        } catch (NumberFormatException e) {
+            System.out.println("Incorrect numeric value");
+        } catch (Exception e) {
+            System.out.println("Other exception");
+        }
+
+    }
+}
