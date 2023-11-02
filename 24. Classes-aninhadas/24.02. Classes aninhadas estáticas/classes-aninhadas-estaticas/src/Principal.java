0d022e0a6421c8323ad27252d6e7dac3c4f6f64c
Moisés Guilherme <moisescomp@gmail.com>
2023-11-02 20:48:36 -0300

    24.2. Classes aninhadas estáticas

--- 24. Classes-aninhadas/24.02. Classes aninhadas estáticas/classes-aninhadas-estaticas/src/Principal.java
+++ 24. Classes-aninhadas/24.02. Classes aninhadas estáticas/classes-aninhadas-estaticas/src/Principal.java
@@ -3,15 +3,32 @@ public class Principal {
     public static void main(String[] args) {
         ServicoEmail servico = new ServicoEmail();
 
-        ServicoEmail.testar();
 
         ServicoEmail.Mensagem mensagem = new ServicoEmail.Mensagem(
                 "joao@algaworks.com", "maria@algaworks.com",
                 "Olá, tudo bem?");
 
+        //ServicoEmail.gerarAssinatura(); // ficou private
         servico.enviar(mensagem);
-        ServicoEmail.Mensagem.testar();
-        ServicoEmail.Mensagem.Mensagem2.Mensagem3.teste3();
+
+        Pessoa joao = new Pessoa();
+        joao.idade = 30;
+        joao.nome = "João";
+
+        System.out.println(joao);
+    }
+
+    public static class Pessoa {
+        int idade;
+        String nome;
+
+        @Override
+        public String toString() {
+            return "Pessoa{" +
+                    "idade=" + idade +
+                    ", nome='" + nome + '\'' +
+                    '}';
+        }
     }
 
 }
--- 24. Classes-aninhadas/24.02. Classes aninhadas estáticas/classes-aninhadas-estaticas/src/ServicoEmail.java
+++ 24. Classes-aninhadas/24.02. Classes aninhadas estáticas/classes-aninhadas-estaticas/src/ServicoEmail.java
@@ -1,11 +1,16 @@
+// Vantagem da classes aninhadas, aumenta o encapsulamento.
+// A classe acessa membros privados internos, membros protegidos
+// A classe Mensagem foi criada para ser usada junto com ServicoEmail
 public class ServicoEmail {
 
     public void enviar(Mensagem mensagem) {
         System.out.printf("Enviando de %s para %s: %s%n", mensagem.getRemetente(), mensagem.getDestinatario(), mensagem.getTexto());
+        // Consegue acessar a variável de instâncias mesmo sendo private
+        System.out.println(mensagem.remetente);
     }
 
-    public static void testar(){
-        System.out.println(">>> Método Estático Testar");
+    private static String gerarAssinatura(){
+        return "\n--\nAlgaWorks";
     }
 
 
@@ -15,10 +20,6 @@ public class ServicoEmail {
         private final String remetente;
         private final String destinatario;
         private final String texto;
-
-        public static void testar(){
-            System.out.println(">>> Método estático testar da classe Mensagem");
-        }
 
         public Mensagem(String remetente, String destinatario, String texto) {
             this.remetente = remetente;
@@ -35,16 +36,9 @@ public class ServicoEmail {
         }
 
         public String getTexto() {
-            return texto;
-        }
-
-        public static class Mensagem2 {
-
-            public static class Mensagem3 {
-                public static void teste3(){
-                    System.out.println(">>> Método estático testar da classe Mensagem 3");
-                }
-            }
+            //ServicoEmail.gerarAssinatura();
+            return texto + gerarAssinatura();
+            // Consegue acessar mesmo se for private da super classe
         }
 
     }
