beb1cf1ae70086b9e6f782a0b963983f6b93f891
Moisés Guilherme <moisescomp@gmail.com>
2023-11-06 06:34:07 -0300

    25.6. Boas práticas: torne as lambdas mais concisas

--- 25. Expressoes-lambda-method-reference/25.01. Introdução ao módulo/crm/src/Principal.java
+++ 25. Expressoes-lambda-method-reference/25.01. Introdução ao módulo/crm/src/Principal.java
@@ -20,25 +20,7 @@ public class Principal {
         cadastroCliente.adicionar(new Cliente("Joaquina", 45));
         cadastroCliente.adicionar(new Cliente("Josefina", 25));
 
-        /*int y = 1;
-        Filtro<Cliente> filtro = new Filtro<Cliente>() {
-            @Override
-            public boolean avaliar(Cliente cliente) {
-                int y = 10; //Classe anônimas pode ter variáveis locais com o mesmo nome de variáveis de outro escopo
-                System.out.println(y);
-                return cliente.getIdade() > 40;
-            }
-        };*/
-
-        int y = 1;
-
-        List<Cliente> clientes = cadastroCliente.consultar((Cliente cliente) -> {
-            // y++; // precisa ser final
-            System.out.println(this.nome); // Consegue pegar o nome pq roda no escopo do método executar
-            boolean resultado = cliente.getIdade() > 40;
-            return resultado;
-        });
-
+        List<Cliente> clientes = cadastroCliente.consultar(cliente -> cliente.getIdade() > 40);
 
         for (Cliente cliente : clientes) {
             System.out.printf("%s - %d%n", cliente.getNome(), cliente.getIdade());
