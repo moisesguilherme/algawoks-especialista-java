135addd2b5904e3ddb089997dcf2f5af2e648dea
Moisés Guilherme <moisescomp@gmail.com>
2023-11-12 13:39:29 -0300

    25.13. Usando Method References - init

---
+++ 25. Expressoes-lambda-method-reference/25.13. Usando Method References/estoque/src/Principal1.java
@@ -0,0 +1,14 @@
+import com.algaworks.estoque.Produto;
+
+import java.util.function.Consumer;
+import java.util.function.ToIntFunction;
+
+public class Principal1 {
+
+	public static void main(String[] args) {
+		ToIntFunction<Produto> function1 = produto -> produto.getQuantidade();
+		Consumer<Produto> consumer1 = produto -> produto.inativar();
+
+	}
+	
+}
