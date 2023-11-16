a0cbf75a5b7baa8e47b73978f794fea4b9fc5df3
Moisés Guilherme <moisescomp@gmail.com>
2023-11-16 07:08:18 -0300

    27.2. Operação intermediária: Stream.filter - início

--- 27. Streams API/27.01. Introdução à Streams API e operações básicas/estoque/src/Principal.java
+++ 27. Streams API/27.01. Introdução à Streams API e operações básicas/estoque/src/Principal.java
@@ -8,29 +8,12 @@ import java.util.stream.Stream;
 public class Principal {
 
     public static void main(String[] args) {
-        // Antes do Java 8, era com loop e interativo
-        // com Java 8, programacão funcional e API (Streams API)
-        // stream uma sequencia de elementos de uma fonte de dados.
-
         var cadastroProduto = new CadastroProduto();
-        //Collections<Produto> -> poderia usar collections no lugar de List
         List<Produto> produtos = cadastroProduto.obterTodos();
 
-        // Forma tradicional
-        /*for (Produto produto : produtos) {
-            produto.ativar();
-            System.out.println(produto);
-        }*/
 
-        // Interface Stream, com elementos da lista (não copia a lista)
-        // stream não duplica
         Stream<Produto> stream = produtos.stream();
-        // Consumer
-        stream.forEach(produto -> {
-            produto.ativar();
-            System.out.println(produto);
-        });
-
+    
     }
 
 }
