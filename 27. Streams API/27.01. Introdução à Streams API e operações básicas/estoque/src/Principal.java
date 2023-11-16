2925416dd46df5d201d90a79b84ebdc1e8a94fab
Moisés Guilherme <moisescomp@gmail.com>
2023-11-16 06:46:51 -0300

    27.1. Introdução à Streams API e operações básicas

---
+++ 27. Streams API/27.01. Introdução à Streams API e operações básicas/estoque/src/Principal.java
@@ -0,0 +1,13 @@
+import com.algaworks.estoque.CadastroProduto;
+import com.algaworks.estoque.Produto;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+    }
+
+}
