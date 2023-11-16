840e32f8a428a26ef8266f8e6d45b414b52ad1ab
Moisés Guilherme <moisescomp@gmail.com>
2023-11-16 07:03:53 -0300

    27.1. Introdução à Streams API e operações básicas - forma tradicional

--- 27. Streams API/27.01. Introdução à Streams API e operações básicas/estoque/src/Principal.java
+++ 27. Streams API/27.01. Introdução à Streams API e operações básicas/estoque/src/Principal.java
@@ -1,13 +1,35 @@
 import com.algaworks.estoque.CadastroProduto;
 import com.algaworks.estoque.Produto;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class Principal {
 
     public static void main(String[] args) {
+        // Antes do Java 8, era com loop e interativo
+        // com Java 8, programacão funcional e API (Streams API)
+        // stream uma sequencia de elementos de uma fonte de dados.
 
+        var cadastroProduto = new CadastroProduto();
+        //Collections<Produto> -> poderia usar collections no lugar de List
+        List<Produto> produtos = cadastroProduto.obterTodos();
+
+        // Forma tradicional
+        for (Produto produto : produtos) {
+            produto.ativar();
+            System.out.println(produto);
+        }
+
+        // Interface Stream, com elementos da lista
+        Stream<Produto> stream = produtos.stream();
+        // Consumer
+        stream.forEach(produto -> {
+            produto.ativar();
+            System.out.println(produto);
+        });
+        
     }
 
 }
