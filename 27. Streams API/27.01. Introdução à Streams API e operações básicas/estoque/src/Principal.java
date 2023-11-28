b6ba28602db5b0889ccca6c0d06d01f274e2f82a
Moisés Guilherme <moisescomp@gmail.com>
2023-11-28 07:07:28 -0300

    27.12. Achatando um Stream com Stream.flatMap - pt1

--- 27. Streams API/27.01. Introdução à Streams API e operações básicas/estoque/src/Principal.java
+++ 27. Streams API/27.01. Introdução à Streams API e operações básicas/estoque/src/Principal.java
@@ -13,18 +13,13 @@ public class Principal {
         var cadastroProduto = new CadastroProduto();
         List<Produto> produtos = cadastroProduto.obterTodos();
 
-        Stream<Stream<Categoria>> stream = produtos.stream()
+        // "Achatar" -> converter vários stream em um único stream
+        Stream<Categoria> stream = produtos.stream()
                .filter(Produto::temEstoque)
-               .map(produto -> produto.getCategorias().stream()); // retorna um stream - Stream<Categoria>
+               .flatMap(produto -> produto.getCategorias().stream());
 
-
-        //stream.forEach(System.out::println);
-        //stream.forEach(obj -> System.out.println(obj.getClass().getName() + " - " + obj.toString()));
-
-        // percorrer
-        // não fica muito bom! Código ruim.
         stream.forEach(obj -> {
-            obj.forEach(System.out::println);
+            System.out.println(obj.getClass().getName() + " - " + obj);;
         });
 
     }
