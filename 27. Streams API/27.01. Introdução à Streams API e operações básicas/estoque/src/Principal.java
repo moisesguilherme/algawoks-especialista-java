3a48f19d872b0a17416dba84439142ff4da7b7f0
Moisés Guilherme <moisescomp@gmail.com>
2023-11-28 07:46:02 -0300

    27.13. Usando as especializações de Stream para tipos primitivos - final

--- 27. Streams API/27.01. Introdução à Streams API e operações básicas/estoque/src/Principal.java
+++ 27. Streams API/27.01. Introdução à Streams API e operações básicas/estoque/src/Principal.java
@@ -5,6 +5,7 @@ import com.algaworks.estoque.Produto;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Principal {
@@ -13,13 +14,14 @@ public class Principal {
         var cadastroProduto = new CadastroProduto();
         List<Produto> produtos = cadastroProduto.obterTodos();
 
-        // Faz o boxing -> transforma o int em Integer
-        Stream<Integer> stream = produtos.stream()
+        // Fluxo de tipos primitivo int
+        IntStream stream = produtos.stream()
                .filter(Produto::temEstoque)
-               .map(Produto::getQuantidade);
+               .mapToInt(Produto::getQuantidade) //ToIntFunction recebe um tipo e retorna um int
+               .sorted();
 
-        // (num * 2) -> faz o unboxing
-        stream.forEach((Integer num) -> System.out.println(num * 2));
+        // (num * 2) -> não vai fazer o unboxing
+        stream.forEach(num -> System.out.println(num * 2));
 
     }
 
