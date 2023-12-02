201d882cfe8c79dca97c55c2dea8bfdefd2b2789
Moisés Guilherme <moisescomp@gmail.com>
2023-12-02 20:58:08 -0300

    20.2. Pool de strings

--- 20. Trabalhando com strings/20.01. Comparação de strings/strings/src/Principal.java
+++ 20. Trabalhando com strings/20.01. Comparação de strings/strings/src/Principal.java
@@ -2,10 +2,16 @@ public class Principal {
 
     public static void main(String[] args) {
         String nome1 = "João";
-        String nome2 = new String("João");
+        String nome2 = "João";
 
-        System.out.println(nome1);
-        System.out.println(nome2);
+        // o hascode é igual
+        System.out.println(nome1.hashCode());
+        System.out.println(nome2.hashCode());
+
+        //String nome2 = new String("João");
+
+        //System.out.println(nome1);
+        //System.out.println(nome2);
 
         //System.out.println(nome1 == nome2); //False (referência endereco de memória)
 
