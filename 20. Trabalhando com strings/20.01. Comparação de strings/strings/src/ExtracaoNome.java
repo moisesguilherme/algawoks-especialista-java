65b5dca9185a2389c1c428c0e25bf060c38c47e6
Moisés Guilherme <moisescomp@gmail.com>
2023-12-03 21:26:38 -0300

    20.5. Extraindo trechos da String com lastIndexOf e substring

--- 20. Trabalhando com strings/20.01. Comparação de strings/strings/src/ExtracaoNome.java
+++ 20. Trabalhando com strings/20.01. Comparação de strings/strings/src/ExtracaoNome.java
@@ -2,8 +2,8 @@ public class ExtracaoNome {
 
     public static void main(String[] args) {
 
-        //String nome = "João Silva Souza";
-        String nome = "João Silva";
+        String nome = "João Silva Souza Magalhães";
+        //String nome = "João Silva";
         //String nome = "João"; // erro
 
         /*System.out.println(nome.indexOf(" "));
@@ -11,9 +11,25 @@ public class ExtracaoNome {
         System.out.println(nome.substring(5, 10));
         */
 
-        System.out.println(obterSobrenome(nome));
-        System.out.println(obterSegundoNome(nome));
+        // pegar o último nome
+        //System.out.println(nome.lastIndexOf(" ")); // do primeiro caracter até o último q possui
+        //System.out.println(nome.substring(11));
 
+
+        //System.out.println(obterSobrenome(nome));
+        //System.out.println(obterSegundoNome(nome));
+        System.out.println(obterUltimoNome(nome));
+
+    }
+
+    private static String obterUltimoNome(String nome) {
+        int posicaoUltimoEspaco = nome.lastIndexOf(" ");
+
+        if(posicaoUltimoEspaco < 0) {
+            throw new RuntimeException("Não é um nome completo");
+        }
+
+        return nome.substring(posicaoUltimoEspaco + 1);
     }
 
     private static String obterSobrenome(String nome) {
@@ -35,10 +51,6 @@ public class ExtracaoNome {
             throw new RuntimeException("Não é um nome completo");
         }
 
- /*       if(posicaoSegundoEspaco < 0) {
-            throw new RuntimeException("Não tem segundo nome");
-        }
-*/
         return nome.substring(posicaoPrimeiroEspaco + 1,
                 posicaoSegundoEspaco < 0 ? nome.length() : posicaoSegundoEspaco);
     }
