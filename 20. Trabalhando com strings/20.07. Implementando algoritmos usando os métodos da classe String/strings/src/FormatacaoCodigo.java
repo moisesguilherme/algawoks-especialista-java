e13be115f9b9f6cdc2dcccdb984f7ca842f39bb5
Moisés Guilherme <moisescomp@gmail.com>
2023-12-04 07:42:14 -0300

    20.7. Implementando algoritmos usando os métodos da classe String - pt2

--- 20. Trabalhando com strings/20.07. Implementando algoritmos usando os métodos da classe String/strings/src/FormatacaoCodigo.java
+++ 20. Trabalhando com strings/20.07. Implementando algoritmos usando os métodos da classe String/strings/src/FormatacaoCodigo.java
@@ -1,10 +1,26 @@
 public class FormatacaoCodigo {
 
     public static void main(String[] args) {
-        int codigo = 123;
+
+        // 000000000 -> 10 caracteres
+        int codigo = 1111123;
+        //long codigo = 12131123131L;
+        // (codigo  + "") virar string ou String.valueOf(codigo)
+
+        String codigoFormatado = preencherEsquerda(
+                String.valueOf(codigo), '0', 10);
+
+        System.out.println(codigoFormatado);
 
     }
 
+    public static String preencherEsquerda(String texto, char caracter, int tamanhoTotal) {
+        if(texto.length() >= tamanhoTotal)
+            return texto;
+
+        return String.valueOf(caracter)
+                .repeat(tamanhoTotal - texto.length()) + texto;
+    }
 
 
 }
