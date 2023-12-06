a9f5b7a6a355106f256be85bd0a229b9da5193c4
Moisés Guilherme <moisescomp@gmail.com>
2023-12-06 08:02:07 -0300

    20.9. Testando correspondências de strings com expressões regulares - com regex

--- 20. Trabalhando com strings/20.09. Testando correspondências de strings com expressões regulares/validacao-email-regex/src/ValidadorEmail.java
+++ 20. Trabalhando com strings/20.09. Testando correspondências de strings com expressões regulares/validacao-email-regex/src/ValidadorEmail.java
@@ -1,7 +1,13 @@
+import java.util.regex.Pattern;
+
 public class ValidadorEmail {
 
     public static boolean validar(String email) {
-        return false;
+        // Passando o email
+        //return Pattern.matches("[\\w.-]+@[a-z\\d.-]+\\.[a-z]{2,3}", email);
+        // Outra forma de verificar usando o método email.matches
+        return email.matches("[\\w.-]+@[a-z\\d.-]+\\.[a-z]{2,3}");
+
     }
 
 }
