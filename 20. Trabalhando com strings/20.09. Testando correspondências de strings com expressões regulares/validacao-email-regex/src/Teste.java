85915f1e827462ac3b82742f24656d6568234e39
Moisés Guilherme <moisescomp@gmail.com>
2023-12-06 08:11:33 -0300

    20.9. Testando correspondências de strings com expressões regulares - regex email mais elaborado - RFC822

--- 20. Trabalhando com strings/20.09. Testando correspondências de strings com expressões regulares/validacao-email-regex/src/Teste.java
+++ 20. Trabalhando com strings/20.09. Testando correspondências de strings com expressões regulares/validacao-email-regex/src/Teste.java
@@ -4,7 +4,9 @@ public class Teste {
 
     public static void main(String[] args) {
 
-        String email = "aaA.111_aaa@al-aga.work.com";
+        //String email = "aaA.111_aaa@al-aga.work.com";
+        String email = "luciano@xxx.com.br.xxx";
+
         //String email = "#aaa111_aaa@alagaworkcom";
         //String email = "@.";
 
@@ -17,9 +19,11 @@ public class Teste {
         // [a-z] -> todas as letras minúscula de a até z
         // [\\w.-]+ -> tudo do \w mais o ponto (.) e o (_) underscore
         // \\d -> dígitos números
-        boolean valido = Pattern.matches("[\\w.-]+@[a-z\\d.-]+\\.[a-z]{2,3}", email);
-
         //boolean valido = Pattern.matches("@\\.", email);  //-> "@."
+        //boolean valido = Pattern.matches("[\\w.-]+@[a-z\\d.-]+\\.[a-z]{2,3}", email);
+
+        // Regex da RFC822
+        boolean valido = Pattern.matches("([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})", email);
 
         System.out.println(valido);
 
