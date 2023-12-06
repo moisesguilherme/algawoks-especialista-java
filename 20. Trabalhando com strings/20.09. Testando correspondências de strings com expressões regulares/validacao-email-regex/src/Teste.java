3c28894f0bf8611eb139e255a96c41795a99151a
Moisés Guilherme <moisescomp@gmail.com>
2023-12-06 19:55:41 -0300

    20.10. Web Scraping: buscando correspondências com Pattern e Matcher

--- 20. Trabalhando com strings/20.09. Testando correspondências de strings com expressões regulares/validacao-email-regex/src/Teste.java
+++ 20. Trabalhando com strings/20.09. Testando correspondências de strings com expressões regulares/validacao-email-regex/src/Teste.java
@@ -20,7 +20,7 @@ public class Teste {
         // [\\w.-]+ -> tudo do \w mais o ponto (.) e o (_) underscore
         // \\d -> dígitos números
         //boolean valido = Pattern.matches("@\\.", email);  //-> "@."
-        //boolean valido = Pattern.matches("[\\w.-]+@[a-z\\d.-]+\\.[a-z]{2,3}", email);
+        boolean valido = Pattern.matches("[\\w.-]+@[a-z\\d.-]+\\.[a-z]{2,3}", email);
 
         // Regex da RFC822
         boolean valido = Pattern.matches("([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})", email);
