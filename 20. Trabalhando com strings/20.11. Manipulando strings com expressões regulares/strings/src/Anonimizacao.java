6a1a4da189b244cfb9cea088bda81294d86c8ef0
Moisés Guilherme <moisescomp@gmail.com>
2023-12-09 19:09:54 -0300

    20.11. Manipulando strings com expressões regulares

---
+++ 20. Trabalhando com strings/20.11. Manipulando strings com expressões regulares/strings/src/Anonimizacao.java
@@ -0,0 +1,17 @@
+public class Anonimizacao {
+
+    public static void main(String[] args) {
+        String html = "<a href=\"mailto:joao@gmail.com\">\n   joao@gmail.com  </a>\n" +
+                "<a>\n   abc@algaworks.com</a><a>xyz@algaworks.com</a>" +
+                "<strong>maria@algaworks.com</strong>";
+
+        String regex = "[\\w.-]+@[a-z\\d.-]+\\.[a-z]{2,3}";
+
+        // replaceFirst
+        String novoHtml = html.replaceAll(regex, "email@anonimizado");
+
+        System.out.println(novoHtml);
+
+    }
+
+}
---
+++ 20. Trabalhando com strings/20.11. Manipulando strings com expressões regulares/strings/src/Split.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Split {
+
+    public static void main(String[] args) {
+        String html = "joao@algaworks.com";
+
+        String[] partes = html.split("[@.]"); //@ e ponto (.)
+
+        System.out.println(Arrays.toString(partes));
+        System.out.println(partes[0] + " da " + partes[1]);
+
+
+    }
+
+}
