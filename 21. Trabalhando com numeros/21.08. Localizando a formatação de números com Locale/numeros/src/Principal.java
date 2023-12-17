ff9f1f73583bf7bbef4207143d563330b9573bde
Moisés Guilherme <moisescomp@gmail.com>
2023-12-17 15:40:32 -0300

    21.8. Localizando a formatação de números com Locale - pt2

--- 21. Trabalhando com numeros/21.08. Localizando a formatação de números com Locale/numeros/src/Principal.java
+++ 21. Trabalhando com numeros/21.08. Localizando a formatação de números com Locale/numeros/src/Principal.java
@@ -12,15 +12,16 @@ public class Principal {
 
         //ISO 639 - lingua
         //ISO 3166 - país
-        Locale.setDefault(new Locale("pt", "BR"));
+        //Locale.setDefault(new Locale("pt", "BR"));
 
         double a = 4_142_298.4238;
         float b = 0.3f;
         int c = -1;
         BigDecimal d = new BigDecimal("45.362");
-        DecimalFormatSymbols symbols = new DecimalFormatSymbols();
-        
-        NumberFormat formatador = new DecimalFormat("¤ #,##0.00;(¤ #,##0.00)");
+
+        // Formatar apenas um decimal format, não global
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols(new Locale("pt", "BR"));
+        NumberFormat formatador = new DecimalFormat("¤ #,##0.00;(¤ #,##0.00)", symbols);
 
         System.out.println(formatador.format(a));
         System.out.println(formatador.format(b));
