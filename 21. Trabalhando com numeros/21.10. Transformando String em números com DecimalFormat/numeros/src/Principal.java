8cc0ba15cd8849f810a1fb118fc0acba0c47ab6b
Moisés Guilherme <moisescomp@gmail.com>
2023-12-17 16:51:35 -0300

    21.10. Transformando String em números com DecimalFormat - pt1

--- 21. Trabalhando com numeros/21.10. Transformando String em números com DecimalFormat/numeros/src/Principal.java
+++ 21. Trabalhando com numeros/21.10. Transformando String em números com DecimalFormat/numeros/src/Principal.java
@@ -2,13 +2,22 @@ import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.text.ParseException;
+import java.util.Locale;
 
 public class Principal {
 
     public static void main(String[] args) throws ParseException {
-        String texto = "1.000,43";
+        Locale.setDefault(new Locale("pt", "BR"));
 
-        double valor = Double.valueOf(texto); //java.lang.NumberFormatException:
+        String texto = "1.000,43";
+        //String texto = "1000.43";
+
+        //double valor = Double.valueOf(texto); //java.lang.NumberFormatException:
+        NumberFormat formatador = new DecimalFormat("#,##0.00");
+        Number valor = formatador.parse(texto);
+
+        System.out.println(valor.getClass());
+        System.out.println(valor);
 
     }
 
