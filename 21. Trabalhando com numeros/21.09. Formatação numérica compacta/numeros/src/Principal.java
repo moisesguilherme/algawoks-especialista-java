c0e8a6ebd1c3b9853145509e818cfeae6bd58478
Moisés Guilherme <moisescomp@gmail.com>
2023-12-17 16:42:40 -0300

    21.10. Transformando String em números com DecimalFormat - erro

--- 21. Trabalhando com numeros/21.09. Formatação numérica compacta/numeros/src/Principal.java
+++ 21. Trabalhando com numeros/21.09. Formatação numérica compacta/numeros/src/Principal.java
@@ -11,9 +11,11 @@ public class Principal {
         BigDecimal d = new BigDecimal("8404142298.0"); // 8 bi
 
         // Java 12
-        // TL DR - (Forma curta)
+        // CLDR: Unicode Common Locale Data Repository
+        // (Forma curta)
         //NumberFormat formatador = NumberFormat.getCompactNumberInstance();
-        NumberFormat formatador = NumberFormat.getCompactNumberInstance(Locale.US, NumberFormat.Style.SHORT);
+        //NumberFormat formatador = NumberFormat.getCompactNumberInstance(Locale.US, NumberFormat.Style.SHORT);
+        NumberFormat formatador = NumberFormat.getCompactNumberInstance(new Locale("pt", "BR"), NumberFormat.Style.SHORT);
 
         System.out.println(formatador.format(a));
         System.out.println(formatador.format(b));
---
+++ 21. Trabalhando com numeros/21.10. Transformando String em números com DecimalFormat/numeros/src/Principal.java
@@ -0,0 +1,15 @@
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+public class Principal {
+
+    public static void main(String[] args) throws ParseException {
+        String texto = "1.000,43";
+
+        double valor = Double.valueOf(texto); //java.lang.NumberFormatException:
+
+    }
+
+}
