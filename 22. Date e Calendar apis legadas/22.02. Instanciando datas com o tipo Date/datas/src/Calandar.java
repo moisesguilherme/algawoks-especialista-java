978506f2470694cdf33cffbd0da08d4fac865c79
Moisés Guilherme <moisescomp@gmail.com>
2023-12-19 07:46:01 -0300

    22.6. Conhecendo o tipo Calendar - pt1

---
+++ 22. Date e Calendar apis legadas/22.02. Instanciando datas com o tipo Date/datas/src/Calandar.java
@@ -0,0 +1,45 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Calandar {
+    public static void main(String[] args) {
+
+        DateFormat formatador = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+        Calendar calendar = Calendar.getInstance();
+
+        System.out.println(calendar); // Verboso
+        // java.util.GregorianCalendar
+        // [time=1702982563448,
+        // areFieldsSet=true,
+        // areAllFieldsSet=true,
+        // lenient=true,
+        // zone=sun.util.calendar.ZoneInfo[
+        // id="America/Sao_Paulo",
+        // offset=-10800000,
+        // dstSavings=0,
+        // useDaylight=false,
+        // transitions=93,
+        // lastRule=null],
+        // firstDayOfWeek=1,
+        // minimalDaysInFirstWeek=1,
+        // ERA=1,
+        // YEAR=2023,
+        // MONTH=11,
+        // WEEK_OF_YEAR=51,
+        // WEEK_OF_MONTH=4,
+        // DAY_OF_MONTH=19,
+        // DAY_OF_YEAR=353,
+        // DAY_OF_WEEK=3,
+        // DAY_OF_WEEK_IN_MONTH=3,
+        // AM_PM=0,HOUR=7,
+        // HOUR_OF_DAY=7,
+        // MINUTE=42,
+        // SECOND=43,
+        // MILLISECOND=448,
+        // ZONE_OFFSET=-10800000,
+        // DST_OFFSET=0]
+
+    }
+}
