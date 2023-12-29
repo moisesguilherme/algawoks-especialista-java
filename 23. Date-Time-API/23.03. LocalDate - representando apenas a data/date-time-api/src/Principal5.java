d3cfee9474a5bddc38fab20454a608a162e64913
Moisés Guilherme <moisescomp@gmail.com>
2023-12-29 07:16:55 -0300

    23.6. Outras formas de obter instâncias de LocalDate, LocalTime e LocalDateTime

---
+++ 23. Date-Time-API/23.03. LocalDate - representando apenas a data/date-time-api/src/Principal5.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class Principal5 {
+
+    public static void main(String[] args) {
+
+        LocalDateTime dataHora = LocalDateTime.now();
+        //LocalDate data = dataHora.toLocalDate();
+        LocalDate data = LocalDate.from(dataHora);
+        System.out.println(data);
+
+
+        //LocalTime hora = dataHora.toLocalTime();
+        LocalTime hora = LocalTime.from(dataHora);
+        System.out.println(data);
+
+    }
+
+}
