2816e9a1ee880b19f26eed4743c5932d7da7977d
Moisés Guilherme <moisescomp@gmail.com>
2024-01-01 16:58:10 -0300

    23.8. Convertendo de String para objetos temporais

---
+++ 23. Date-Time-API/23.08. Convertendo de String para objetos temporais/date-time-api/src/ExemploLocalTime.java
@@ -0,0 +1,10 @@
+import java.time.LocalTime;
+
+public class ExemploLocalTime {
+
+    public static void main(String[] args) {
+        LocalTime horario = LocalTime.parse("19:45:20");
+        System.out.println(horario);
+    }
+
+}
