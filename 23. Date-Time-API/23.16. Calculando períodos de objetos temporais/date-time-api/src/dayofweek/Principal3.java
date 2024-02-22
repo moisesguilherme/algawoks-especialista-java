d25caf339527f2148790b0a9e7abd46a843a9e82
Moisés Guilherme <moisescomp@gmail.com>
2024-02-22 07:40:34 -0300

    23.21. Year: representando o ano

---
+++ 23. Date-Time-API/23.16. Calculando períodos de objetos temporais/date-time-api/src/dayofweek/Principal3.java
@@ -0,0 +1,22 @@
+package dayofweek;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Year;
+
+public class Principal3 {
+    public static void main(String[] args) {
+
+        //Year ano = Year.now();
+        //Year ano = Year.of(2023);
+        //Year ano = Year.from(LocalDate.now());
+        Year ano = Year.parse("2024");
+
+        System.out.println(ano);
+        System.out.println(ano.isLeap());
+
+        LocalDate diaDoProgramador = ano.atDay(256);
+        System.out.println(diaDoProgramador);
+
+    }
+}
