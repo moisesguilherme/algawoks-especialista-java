7cc7e3268e76932602dba73854f00705c8740b7c
Moisés Guilherme <moisescomp@gmail.com>
2024-02-23 08:17:09 -0300

    23.22. Month: representando o mês

---
+++ 23. Date-Time-API/23.16. Calculando períodos de objetos temporais/date-time-api/src/month/Principal3.java
@@ -0,0 +1,23 @@
+package month;
+
+import java.time.Month;
+
+public class Principal3 {
+
+
+    public static void main(String[] args) {
+
+        System.out.println(Month.DECEMBER.firstDayOfYear(true));
+        System.out.println(Month.DECEMBER.firstDayOfYear(false));
+
+
+        System.out.println(Month.FEBRUARY.length(true));
+        System.out.println(Month.FEBRUARY.length(false));
+
+        // primeiro mes do trimestre
+        System.out.println(Month.FEBRUARY.firstMonthOfQuarter());
+        System.out.println(Month.SEPTEMBER.firstMonthOfQuarter());
+
+    }
+
+}
