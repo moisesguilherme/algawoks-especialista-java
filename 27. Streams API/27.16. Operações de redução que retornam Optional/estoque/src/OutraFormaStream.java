a949623ae570e20f7d4832458aa50a8b5b56a27e
Moisés Guilherme <moisescomp@gmail.com>
2024-05-07 22:08:11 -0300

    27.26. Outras formas de obter instâncias de Stream

--- 27. Streams API/27.16. Operações de redução que retornam Optional/estoque/src/OutraFormaStream.java
+++ 27. Streams API/27.16. Operações de redução que retornam Optional/estoque/src/OutraFormaStream.java
@@ -1,5 +1,7 @@
+import javax.sound.midi.Soundbank;
 import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.Random;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -7,7 +9,7 @@ public class OutraFormaStream {
 
     public static void main(String[] args) {
 
-        String[] nomes = { "Maria", "Sebastiaão", "João"};
+        /*String[] nomes = { "Maria", "Sebastiaão", "João"};
         Arrays.stream(nomes)
                 .map(String::toUpperCase)
                 .sorted()
@@ -28,5 +30,43 @@ public class OutraFormaStream {
                 .sum();
 
         System.out.println(total);
+        */
+
+        /*
+        Stream<Integer> stream1 = Stream.of(1, 2, 3);
+        Stream<Integer> stream2 = Stream.of(9, 8, 7);
+
+        Stream.concat(stream1, stream2)
+                .filter(x -> x%2 == 0) //somente números pares
+                .forEach(System.out::print);
+        */
+
+        /*
+        IntStream.range(1, 10)
+                .forEach(System.out::print); // 123456789 até nove
+        */
+
+        /*
+        IntStream.rangeClosed(1, 10)
+                .skip(2) //salta 2 -> 345678910
+                .forEach(System.out::print);
+         */
+
+        Random random = new Random();
+        int[] contador = {0};
+
+        Stream.generate(() -> random.nextInt(99) + 1)
+                .peek(System.out::println)
+                .filter(x -> x%2 == 0)
+                .distinct()
+                .limit(10)
+                .forEach( numero -> {
+                    contador[0]++;
+                    System.out.println("Número " + contador[0] + ": " + numero);
+                });
+
     }
+
+
+
 }
