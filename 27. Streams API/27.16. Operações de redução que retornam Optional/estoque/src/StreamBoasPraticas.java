af5b44d06817dab7f92847f8c11d807b25a49a41
Moisés Guilherme <moisescomp@gmail.com>
2024-05-09 07:47:54 -0300

    27.28. Boas práticas: prefira funções em streams sem efeito colateral - pt1

---
+++ 27. Streams API/27.16. Operações de redução que retornam Optional/estoque/src/StreamBoasPraticas.java
@@ -0,0 +1,38 @@
+import com.algaworks.estoque.CadastroProduto;
+import com.algaworks.estoque.Fabricante;
+import com.algaworks.estoque.Produto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public class StreamBoasPraticas {
+
+    public static void main(String[] args) {
+        // Boas práticas uso do Stream
+
+        var cadastroProduto = new CadastroProduto();
+        List<Produto> produtos = cadastroProduto.obterTodos();
+
+        List<Fabricante> fabricantes = new ArrayList<>();
+
+
+        // Sempre q for trabalhar com stream não deve gerar
+        // função que vc passa como operação, não deve gerar efeito colateral com estado da aplicação
+        // (alterar uma variável externa)
+
+        // não pode ter dependência de variável externa
+
+        produtos.stream()
+                .filter(Produto::temEstoque)
+                .forEach(produto -> {
+                    if(!fabricantes.contains(produto.getFabricante())){
+                        fabricantes.add(produto.getFabricante()); //Altera o estado de um objeto, de uma variável externa
+                    }
+                });
+
+        System.out.println(fabricantes);
+    }
+
+}
