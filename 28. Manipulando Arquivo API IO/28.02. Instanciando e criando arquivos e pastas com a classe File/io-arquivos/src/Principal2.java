cb2c28117a6586c5972e4d347b3683ff77bb3b13
Moisés Guilherme <moisescomp@gmail.com>
2024-05-13 21:46:54 -0300

    28.3. Obtendo o caminho absoluto e canônico de File

---
+++ 28. Manipulando Arquivo API IO/28.02. Instanciando e criando arquivos e pastas com a classe File/io-arquivos/src/Principal2.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.IOException;
+
+public class Principal2 {
+
+    public static void main(String[] args) throws IOException {
+
+        File arquivo1 = new File("/home/moises/Desktop/DEV/AlgaWorks/EspecialistaJava/28. Manipulando Arquivo API IO/28.02. Instanciando e criando arquivos e pastas com a classe File/docs/contrato.txt");
+        System.out.println(arquivo1);
+        System.out.println("----");
+
+        File arquivo2 = new File("docs/contrato.txt");
+        System.out.println(arquivo2);
+        System.out.println(arquivo2.getAbsoluteFile());
+        System.out.println("----");
+
+        File arquivo3 = new File("../docs/contrato.txt");
+        System.out.println(arquivo3);
+        System.out.println(arquivo3.getAbsoluteFile());
+        System.out.println(arquivo3.getCanonicalFile()); // agora ele volta ../ "resolve o diretório"
+
+
+
+    }
+
+}
