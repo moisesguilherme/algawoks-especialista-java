f7eb3762fd7834094832956dd836d618c6c046b4
Moisés Guilherme <moisescomp@gmail.com>
2024-05-14 21:44:54 -0300

    28.5. Obtendo informações de arquivos e diretórios

--- 28. Manipulando Arquivo API IO/28.02. Instanciando e criando arquivos e pastas com a classe File/io-arquivos/src/Principal.java
+++ 28. Manipulando Arquivo API IO/28.02. Instanciando e criando arquivos e pastas com a classe File/io-arquivos/src/Principal.java
@@ -5,11 +5,11 @@ public class Principal {
 
     public static void main(String[] args) throws IOException {
 
-
         File pasta1 = new File("/home/moises/Desktop/DEV/AlgaWorks/EspecialistaJava/28. Manipulando Arquivo API IO/28.02. Instanciando e criando arquivos e pastas com a classe File/docs");
         System.out.printf("Pasta 1 criada: %b%n", pasta1.mkdir());
         //pasta1.mkdir();
 
+        /*
         File pasta2 = new File("/home/moises/Desktop/DEV/AlgaWorks/EspecialistaJava/28. Manipulando Arquivo API IO/28.02. Instanciando e criando arquivos e pastas com a classe File/fotos/viagem");
         System.out.printf("Pasta 2 criada: %b%n", pasta2.mkdirs());
 
@@ -22,6 +22,7 @@ public class Principal {
 
         System.out.printf("Arquivo 4 criado: %b%n", arquivo4.createNewFile());
         //arquivo4.createNewFile();// precisa lançar a exceção
+         */
     }
 
 }
