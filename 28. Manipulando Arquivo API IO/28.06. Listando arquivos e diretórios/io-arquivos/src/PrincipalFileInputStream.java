79cf0ed8ffc5e3ac452851c63adf6529e320030c
Moisés Guilherme <moisescomp@gmail.com>
2024-05-16 22:09:34 -0300

    28.9. Boa prática: tratando IOException com try-with-resources

--- 28. Manipulando Arquivo API IO/28.06. Listando arquivos e diretórios/io-arquivos/src/PrincipalFileInputStream.java
+++ 28. Manipulando Arquivo API IO/28.06. Listando arquivos e diretórios/io-arquivos/src/PrincipalFileInputStream.java
@@ -4,37 +4,18 @@ public class PrincipalFileInputStream {
 
     public static void main(String[] args) {
         File arquivoOrigem = new File("docs/contrato.txt");
-        //InputStream inputStream = null;
-        //Declarar no try
-        // pode colocar mais de um new
-        try (InputStream inputStream = new FileInputStream(arquivoOrigem);
-             InputStream inputStream2 = new FileInputStream(arquivoOrigem)) {
-            //inputStream = new FileInputStream(arquivoOrigem);
+
+        // try-with-resource (ele vai ser fechado no final)
+        try (InputStream inputStream = new FileInputStream(arquivoOrigem)) {
             int conteudo;
 
             while ((conteudo = inputStream.read()) != -1) {
                 System.out.print((char) conteudo); //problema com acento exemplo: ú
             }
-
-            //System.out.println(inputStream.read()); //69
-            //System.out.println(inputStream.read());
-
         } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Arquivo não encontrado", e);
         } catch (IOException e) {
-            /// Se der problema no read
-            throw new RuntimeException(e);
-        }/* finally {
-            if (inputStream != null) {
-                try {
-                    inputStream.close();
-                } catch (IOException e) {
-                    System.out.println("Não foi possível fechar fluxo com arquivo");
-                    // se não fechar o fluxo, não tem muito oq fazer...
-                    //throw new RuntimeException(e);
-                }
-            }
-        }*/
-        // Classe AutoCloseble -> usada para try-with-resources
+            throw new RuntimeException("Erro de I/O", e);
+        }
     }
 }
