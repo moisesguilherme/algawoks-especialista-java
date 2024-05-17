ab490b8af4f3fc74a2739d2abddc9af62b869d17
Moisés Guilherme <moisescomp@gmail.com>
2024-05-16 21:46:51 -0300

    28.8. Lendo arquivos com FileInputStream

---
+++ 28. Manipulando Arquivo API IO/28.06. Listando arquivos e diretórios/io-arquivos/src/PrincipalFileInputStream.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+public class PrincipalFileInputStream {
+
+    public static void main(String[] args) {
+        File arquivoOrigem = new File("docs/contrato.txt");
+        InputStream inputStream = null;
+
+        try {
+            inputStream = new FileInputStream(arquivoOrigem);
+
+            int conteudo;
+
+            while ((conteudo = inputStream.read()) != -1) {
+                System.out.print((char) conteudo); //problema com acento exemplo: ú
+            }
+
+            //System.out.println(inputStream.read()); //69
+            //System.out.println(inputStream.read());
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            /// Se der problema no read
+            throw new RuntimeException(e);
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    System.out.println("Não foi possível fechar fluxo com arquivo");
+                    // se não fechar o fluxo, não tem muito oq fazer...
+                    //throw new RuntimeException(e);
+                }
+            }
+        }
+
+    }
+}
