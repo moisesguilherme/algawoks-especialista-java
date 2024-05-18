70470fcdae7e51712e11285eb07046fa2683d4dd
Moisés Guilherme <moisescomp@gmail.com>
2024-05-17 21:35:32 -0300

    28.9. Boa prática: tratando IOException com try-with-resources - pt2

--- 28. Manipulando Arquivo API IO/28.06. Listando arquivos e diretórios/io-arquivos/src/PrincipalFileInputStream.java
+++ 28. Manipulando Arquivo API IO/28.06. Listando arquivos e diretórios/io-arquivos/src/PrincipalFileInputStream.java
@@ -2,11 +2,16 @@ import java.io.*;
 
 public class PrincipalFileInputStream {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         File arquivoOrigem = new File("docs/contrato.txt");
 
+        // com java 9 consegue fazer com final.
+        final InputStream inputStream = new FileInputStream(arquivoOrigem);
+        final InputStream inputStream2 = new FileInputStream(arquivoOrigem);
+
         // try-with-resource (ele vai ser fechado no final)
-        try (InputStream inputStream = new FileInputStream(arquivoOrigem)) {
+        // Uma boa prática, sempre usar.
+        try (inputStream; inputStream2) {
             int conteudo;
 
             while ((conteudo = inputStream.read()) != -1) {
