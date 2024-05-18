1bc682d69dcc391302eee590bdfb48f7572b7841
Moisés Guilherme <moisescomp@gmail.com>
2024-05-17 21:43:45 -0300

    28.10. Escrevendo arquivos com FileOutputStream pt1

--- 28. Manipulando Arquivo API IO/28.06. Listando arquivos e diretórios/io-arquivos/src/PrincipalFileInputStream.java
+++ 28. Manipulando Arquivo API IO/28.06. Listando arquivos e diretórios/io-arquivos/src/PrincipalFileInputStream.java
@@ -2,21 +2,20 @@ import java.io.*;
 
 public class PrincipalFileInputStream {
 
-    public static void main(String[] args) throws FileNotFoundException {
-        File arquivoOrigem = new File("docs/contrato.txt");
+    public static void main(String[] args) {
+        File arquivoOrigem = new File("docs/foto1.jpg");
+        File arquivoDestino = new File("docs/foto2.jpg");
 
-        // com java 9 consegue fazer com final.
-        final InputStream inputStream = new FileInputStream(arquivoOrigem);
-        final InputStream inputStream2 = new FileInputStream(arquivoOrigem);
+        try (InputStream inputStream = new FileInputStream(arquivoOrigem);
+             OutputStream outputStream = new FileOutputStream(arquivoDestino)) {
 
-        // try-with-resource (ele vai ser fechado no final)
-        // Uma boa prática, sempre usar.
-        try (inputStream; inputStream2) {
             int conteudo;
 
             while ((conteudo = inputStream.read()) != -1) {
-                System.out.print((char) conteudo); //problema com acento exemplo: ú
+                System.out.println(conteudo);
+                outputStream.write(conteudo);
             }
+
         } catch (FileNotFoundException e) {
             throw new RuntimeException("Arquivo não encontrado", e);
         } catch (IOException e) {
