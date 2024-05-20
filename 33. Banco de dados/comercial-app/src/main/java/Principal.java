d52dd62e95f4345f2fc292b216025f4101e82dfa
Moisés Guilherme <moisescomp@gmail.com>
2024-05-20 18:42:57 -0300

    33.9. Executando consultas SQL com PreparedStatement

--- 33. Banco de dados/comercial-app/src/main/java/Principal.java
+++ 33. Banco de dados/comercial-app/src/main/java/Principal.java
@@ -11,48 +11,32 @@ public class Principal {
         System.out.printf("Pesquisa por nome: ");
         String nomePesquisa = scanner.nextLine();
 
-        //Connection é um AutoCloseble
-        //try-with-research
+
+
         try (Connection conexao = DriverManager
                 .getConnection("jdbc:mysql://localhost:3306/ej_comercial", "root", "Root@123");
-             Statement comando = conexao.createStatement();
-             // ResultSet é fechado automaticamente, quando o Statement é fechado
-             // não é uma boa prática concatenar a string com select, problema com ' e SQL Injection
-             ResultSet resultado = comando.executeQuery(
-                     "select * from venda where nome_cliente like '%"
-                             + nomePesquisa + "%'");
+             //PreparedStatement, já fica compilado, esperando a execução
+             //o Statement é executado a cada execução.
+             PreparedStatement comando = conexao.prepareStatement("select * from venda where nome_cliente like ?");
         ) {
-            // Ideal no try
-            //Statement comando = conexao.createStatement();
-            //ResultSet resultado = comando.executeQuery("select * from venda");
+
+            comando.setString(1, "%" + nomePesquisa + "%"); //indice 1, 2 (segundo parâmetro)
+            ResultSet resultado = comando.executeQuery(); // Não é AutoClosable
 
             while(resultado.next()) {
                 Long id = resultado.getLong("id");
                 String nomeCliente = resultado.getString("nome_cliente");
                 BigDecimal valorTotal = resultado.getBigDecimal("valor_total");
-                // Não é o Date do Java, sim do java.sql
                 Date dataPagamento = resultado.getDate("data_pagamento");
 
                 System.out.printf("%d - %s R$%.2f %s%n",
                         id, nomeCliente, valorTotal, dataPagamento);
             }
 
-            // ideal é no try
-            //comando.close();
-            //resultado.close();
-            /*
-            System.out.println(conexao.getClass());
-            System.out.println(comando.getClass());
-            System.out.println(resultado.getClass());
-             */
         } catch (SQLException e) {
             System.out.println("Erro de banco de dados");
             e.printStackTrace();
-            //throw new RuntimeException(e);
         }
-
-
-
     }
 
 }
