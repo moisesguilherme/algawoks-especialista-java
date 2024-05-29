0701a21c2342d1c6ba0428546454738ffdad54ad
Moisés Guilherme <moisescomp@gmail.com>
2024-05-29 07:02:31 -0300

    30.6. Entendendo e gerando serialVersionUID

--- 30. Serialização de objetos/30.02. Tornando classes serializáveis com a interface Serializable/serializacao/src/Funcionario.java
+++ 30. Serialização de objetos/30.02. Tornando classes serializáveis com a interface Serializable/serializacao/src/Funcionario.java
@@ -1,3 +1,4 @@
+import java.io.Serial;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -5,17 +6,26 @@ import java.time.LocalDate;
 // Serializable é uma interface de marcação não tem nenhum método
 public class Funcionario implements Serializable {
 
+    @Serial
+    private static final long serialVersionUID = 1;
+
+    private Integer codigo;
     private String nome;
     private LocalDate dataNascimento;
     // propriedade transient (não vai serializar essa propriedade)
     // não vai salvar esse dado e vai desserializar vai aparecer como null
     // salario=null
-    private transient BigDecimal salario;
+    //private transient BigDecimal salario;
+    private BigDecimal salario;
 
     public Funcionario(String nome, LocalDate dataNascimento, BigDecimal salario) {
         this.nome = nome;
         this.dataNascimento = dataNascimento;
         this.salario = salario;
+    }
+
+    public void testar() {
+
     }
 
     public String getNome() {
@@ -42,13 +52,22 @@ public class Funcionario implements Serializable {
         this.salario = salario;
     }
 
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
     @Override
     public String toString() {
         return "Funcionario{" +
-                "nome='" + nome + '\'' +
+                "codigo=" + codigo +
+                ", nome='" + nome + '\'' +
                 ", dataNascimento=" + dataNascimento +
                 ", salario=" + salario +
                 '}';
     }
-    
+
 }
